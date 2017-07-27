d6b7f7db7a14e3583bcbb7b10984a4fb4ba6d87f
perakerberg <per.akerberg@evolvetechnology.se>
2017-07-27 11:03:17 +0200

    Fått igång tester och "lagat" wallaby med nya referenser i package.json

---
+++ tests/services/modalService.spec.ts
@@ -0,0 +1,34 @@
+import { ModalService, ModalConfiguration } from "../../component-package/services/modalService";
+
+describe("ModalService", () => {
+
+    var modalService: ModalService;
+    var publishedModals: ModalConfiguration[]
+    var dialogResult: string;
+    beforeEach(() => {
+        modalService = new ModalService();
+        publishedModals = [];
+        modalService.modalOpened$.subscribe(x => publishedModals.push(x));
+    });
+    describe("When a single button dialog is opened", () => {
+        beforeEach(() => {
+            modalService.openOneButtonDialog("DialogTitle", "DialogMessage", "Ok", () => dialogResult = "Ok");
+        });
+        it("a modal event is published", () => {
+            expect(publishedModals.length).toBe(1);
+        });
+        it("The published event contains one button", () => {
+            expect(publishedModals[0].buttons.length).toBe(1);
+        });
+        it("The button's text is OK", () => {
+            expect(publishedModals[0].buttons[0].text).toBe("Ok");
+        });
+        describe("When the button is clicked", () => {
+            it("The callback is called", () => {
+                publishedModals[0].buttons[0].callback();
+                expect(dialogResult).toBe("Ok");
+            });
+
+        });
+    });
+});
