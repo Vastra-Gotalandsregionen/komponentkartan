c815c86aa053f05d432c07f96ccfa841ff3ef6ac
Frosthage <mikael.frosthage@consid.se>
2017-08-09 12:06:23 +0200

    Notera import * as $ from 'jquery';   på rad 19.  ng-kompilatorn förstår inte $ i de andra komponenterna om den inte refereras i app-modulen.

---
+++ angular-cli-app/app/app.module.ts
@@ -0,0 +1,38 @@
+﻿import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core"
+import { BrowserModule, Title } from "@angular/platform-browser"
+
+import { FormsModule } from "@angular/forms";
+
+import { KomponentkartanApplicationComponent } from "./app.component";
+
+import { KomponentkartaComponent } from '../../demo-app/komponentkarta/komponentkarta.component'
+
+import { FormatmallComponent } from "../../demo-app/formatmall/formatmall.component"
+import { FargkartaComponent } from "../../demo-app/fargkarta/fargkarta.component"
+import { MenuSelectorComponent } from "../../demo-app/menuSelector/menuSelector.component"
+
+import { KomponentkartanModule } from "../../component-package/komponentkartan.module";
+
+import { RouterModule, Routes } from "@angular/router"
+import { appRoutes } from "./routes"
+
+import * as $ from 'jquery';
+
+@NgModule({
+    imports: [
+        KomponentkartanModule,
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    declarations: [
+        KomponentkartanApplicationComponent,
+        KomponentkartaComponent,
+        FormatmallComponent,
+        FargkartaComponent,
+        MenuSelectorComponent
+    ],
+
+    bootstrap: [KomponentkartanApplicationComponent]
+})
+export class AppModule { }
