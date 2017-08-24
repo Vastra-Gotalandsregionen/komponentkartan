cf3418a25f3ec6f23d7156e5105b7fce466a5448
Caroline Bornsjö <caroline.bornsjo@ikaros.se>
2017-08-24 13:32:22 +0200

    Skapat en basklass för dropdown komponenterna

---
+++ component-package/controls/dropdown-base/dropdown.base.component.ts
@@ -0,0 +1,129 @@
+import { Component, Input, AfterViewInit, ElementRef, Output, EventEmitter, ViewChild, SimpleChanges } from "@angular/core";
+import { IDropdownItem } from "../../models/dropdownItem.model";
+import { FilterPipe } from "../../pipes/filterPipe";
+import { DropdownItemToSelectedTextPipe } from "../../pipes/dropdownItemToSelectedTextPipe";
+import { FilterTextboxComponent } from "../filterTextbox/filterTextbox.component";
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
+
+
+@Component({
+
+})
+
+export class DropdownBaseComponent {
+    @Input() selectAllSelectedText: string;
+    @Input() selectAllItemText: string;
+    @Input() filterProperties: string[];
+
+    @ViewChild(FilterTextboxComponent) filterTextboxComponent: FilterTextboxComponent;
+    @ViewChild(PerfectScrollbarComponent) scrollbarComponent: PerfectScrollbarComponent;
+
+    selectAllItem: IDropdownItem;
+    expanded: boolean;
+    filterVisible: boolean;
+    scrollVisible: boolean;
+    filter: string;
+
+    protected filterLimit = 20;
+    protected scrollLimit = 8;
+    protected filterPipe: FilterPipe;
+    protected preventCollapse: boolean;
+
+
+    protected _items: IDropdownItem[];
+    @Input() set items(value: IDropdownItem[]) {
+        //The scrollbar component would not refresh when items were changed unless we added a timeout...
+        //Ugly solution for sure, but until a better one comes along it will have to do :(
+        this._items = value;
+        setTimeout(() => {
+            this.scrollbarComponent.update();
+
+            this.listenToScrollbarEvents();
+
+        }, 500);
+    }
+    get items(): IDropdownItem[] {
+
+        return this._items;
+    }
+
+    private listenToScrollbarEvents() {
+        $(this.scrollbarComponent.elementRef.nativeElement).scroll((e) => {
+            this.hideDimmersIfScrollIsAtBottomOrTop(e);
+        });
+    }
+
+    constructor(protected elementRef: ElementRef) {
+        this.expanded = false;
+        this.filterVisible = false;
+        this.scrollVisible = false;
+        this.filterPipe = new FilterPipe();
+    }
+
+    private hideDimmersIfScrollIsAtBottomOrTop(scrollEvent: JQueryEventObject) {
+        let scrollbar = $(scrollEvent.target);
+        let margintolerance = 20;
+
+        var scrollHeight = scrollEvent.target.scrollHeight - margintolerance;
+        var clientHeight = scrollEvent.target.clientHeight;
+        var scrollTop = scrollEvent.target.scrollTop;
+
+        if (clientHeight + scrollTop >= scrollHeight) {
+            scrollbar.next('.dropdown__dimmer--bottom').hide();
+        }
+        else {
+            scrollbar.next('.dropdown__dimmer--bottom').show();
+        }
+
+        if (scrollTop === 0) {
+            scrollbar.prev('.dropdown__dimmer--top').hide();
+        }
+        else {
+            scrollbar.prev('.dropdown__dimmer--top').show();
+        }
+    }
+
+
+    filterItems(filterValue: string) {
+        this.filter = filterValue;
+        this.updateScrolled();
+        //Scroll to top when filter is changed
+        $(".dropdown__menu__items").scrollTop(0);
+    }
+
+    updateScrolled() {
+
+        if (!this.items) {
+            this.scrollVisible = false;
+            return;
+        }
+
+        var visibleItemCount = this.filterPipe.transform(this.items, this.filter, ["displayName"]).length;
+
+        this.scrollVisible = visibleItemCount > this.scrollLimit;
+    }
+
+    toggleExpand(event: Event) {
+        if (this.preventCollapse) {
+            event.cancelBubble = true;
+            event.returnValue = false;
+            this.preventCollapse = false;
+            return;
+        }
+        let target = event.target || event.srcElement || event.currentTarget;
+        let element = $(target);
+        if (!element.is("input") && !element.is(".scroll-bar")) {
+            this.expanded = !this.expanded;
+        }
+    }
+
+    onDocumentClick(event: any) {
+
+        let target = event.target || event.srcElement || event.currentTarget;
+
+        if (!this.elementRef.nativeElement.contains(target)) {
+            this.expanded = false;
+        }
+    }
+
+}
--- component-package/controls/dropdown-multiselect/dropdown-multiselect.component.ts
+++ component-package/controls/dropdown-multiselect/dropdown-multiselect.component.ts
@@ -1,208 +1,37 @@
-
-
-import { Component, Input, AfterViewInit, ElementRef, OnChanges, Output, EventEmitter, OnInit, ViewChild, SimpleChanges } from "@angular/core";
+import { Component, Input, AfterViewInit, ElementRef, OnChanges, Output, EventEmitter, ViewChild, SimpleChanges } from "@angular/core";
 import { IDropdownItem } from "../../models/dropdownItem.model";
 import { FilterPipe } from "../../pipes/filterPipe";
 import { DropdownItemToSelectedTextPipe } from "../../pipes/dropdownItemToSelectedTextPipe";
 import { FilterTextboxComponent } from "../filterTextbox/filterTextbox.component";
 import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
-
-//This is the style for the scrollbar. This was the only wa we could override the inline styles set by the component itself
-var scrollbarStyle = `
-
-:host /deep/ .ps {
-    -ms-touch-action: auto;
-    touch-action: auto;
-    overflow: hidden !important;
-    -ms-overflow-style: none;
-    z-index:50;
-}
-
-:host /deep/ .ps.ps--active-y>.ps__scrollbar-y-rail {
-    display: block;
-    background-color: transparent;
-}
-
-:host /deep/ .ps.ps--in-scrolling.ps--y>.ps__scrollbar-y-rail {
-    background-color: transparent !important;
-    opacity: .9;
-}
-
-:host /deep/ .ps.ps--in-scrolling.ps--y>.ps__scrollbar-y-rail>.ps__scrollbar-y {
-    background-color: transparent !important;
-    width: 11px;
-    min-height: 40px;
-}
-
-
-
-:host /deep/ .ps>.ps__scrollbar-y-rail {
-    display: none;
-    position: absolute;
-    opacity: 0;
-    -webkit-transition: background-color .2s linear, opacity .2s linear;
-    -o-transition: background-color .2s linear, opacity .2s linear;
-    -moz-transition: background-color .2s linear, opacity .2s linear;
-    transition: background-color .2s linear, opacity .2s linear;
-    right: 0;
-    width: 11px;
-}
-
-:host /deep/ .ps>.ps__scrollbar-y-rail>.ps__scrollbar-y {
-    position: absolute;
-    background-color: #aaa;
-    -webkit-border-radius: 6px;
-    -moz-border-radius: 6px;
-    border-radius: 6px;
-    -webkit-transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, -webkit-border-radius .2s ease-in-out;
-    transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, -webkit-border-radius .2s ease-in-out;
-    -o-transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, border-radius .2s ease-in-out;
-    -moz-transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, border-radius .2s ease-in-out, -moz-border-radius .2s ease-in-out;
-    transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, border-radius .2s ease-in-out;
-    transition: background-color .2s linear, height .2s linear, width .2s ease-in-out, border-radius .2s ease-in-out, -webkit-border-radius .2s ease-in-out, -moz-border-radius .2s ease-in-out;
-    right: 2px;
-    width: 8px;
-    min-height: 40px;
-
-
-}
-
-:host /deep/ .ps>.ps__scrollbar-y-rail:hover>.ps__scrollbar-y,
-:host /deep/ .ps>.ps__scrollbar-y-rail:active>.ps__scrollbar-y {
-    width: 8px;
-    min-height: 40px;
-
-}
-
-:host /deep/ .ps:hover.ps--in-scrolling.ps--y>.ps__scrollbar-y-rail {
-    background-color: transparent;
-    opacity: .9;
-}
-
-:host /deep/ .ps:hover.ps--in-scrolling.ps--y>.ps__scrollbar-y-rail>.ps__scrollbar-y {
-    width: 8px;
-}
-
-:host /deep/ .ps:hover>.ps__scrollbar-y-rail {
-    opacity: .6;
-}
-
-
-:host /deep/ .ps:hover>.ps__scrollbar-y-rail:hover {
-    background-color: transparent;
-    opacity: .9;
-}
-
-:host /deep/ .ps:hover>.ps__scrollbar-y-rail:hover>.ps__scrollbar-y {
-    background-color: #999;
-}
-
-:host /deep/ .ps.ps--active-y>.ps__scrollbar-y-rail>.ps__scrollbar-y {
-    background-color: #999;
-}
-
-:host /deep/ .ps.ps--in-scrolling.ps--y>.ps__scrollbar-y-rail>.ps__scrollbar-y {
-    background-color: #999;
-}
-
-
-:host /deep/ .ps {
-    position: relative;
-    display: block;
-}
-
-:host /deep/ .ps[hidden] {
-    display: none;
-}
-
-:host /deep/ .ps[fxlayout]>.ps-content {
-    display: flex;
-    -webkit-box-flex: 1;
-    -ms-flex: 1 1 auto;
-    flex: 1 1 auto;
-}
-
-:host /deep/ .ps.ps-static {
-    position: static;
-}
-
-:host /deep/ .ps.ps-static>.ps__scrollbar-y-rail {
-    top: 0 !important;
-}
-
-:host /deep/ .ps.ps-outside.ps--active-y {
-    padding-right: 24px;
-    margin-right: -24px;
-}
-
-:host /deep/ .ps.ps-outside>.ps__scrollbar-x-rail {
-    margin: 0 8px;
-}
-
-:host /deep/ .ps.ps-outside>.ps__scrollbar-y-rail {
-    margin: 8px 0;
-}`;
+import { DropdownBaseComponent } from "../dropdown-base/dropdown.base.component";
 
 @Component({
     selector: "vgr-dropdown-multiselect",
     moduleId: module.id,
     templateUrl: "./dropdown-multiselect.component.html",
     host: { '(document:click)': "onDocumentClick($event)" },
-    styles: [scrollbarStyle]
+    styleUrls: ["../dropdown-base/dropdown.scrollbar.css"]
 })
-export class DropdownMultiselectComponent implements OnChanges {
-    @Input() selectAllSelectedText: string;
-    @Input() selectAllItemText: string;
-    @Input() displayAllItemsText: string;
-    @Input() filterProperties: string[];
-    @Output() selectionChanged = new EventEmitter<IDropdownItem[]>();
-    @ViewChild(FilterTextboxComponent) filterTextboxComponent: FilterTextboxComponent;
-    @ViewChild(PerfectScrollbarComponent) scrollbarComponent: PerfectScrollbarComponent;
-    selectAllItem: IDropdownItem;
-    expanded: boolean;
-    filterVisible: boolean;
-    scrollVisible: boolean;
-    filter: string;
-    dropdownText: string;
-    private filterLimit = 20;
-    private scrollLimit = 8;
-    private filterPipe: FilterPipe;
-    private preventCollapse: boolean;
-    private _items: IDropdownItem[];
-    @Input() set items(value: IDropdownItem[]) {
-        //The scrollbar component would not refresh when items were changed unless we added a timeout...
-        //Ugly solution for sure, but until a better one comes along it will have to do :(
-        this._items = value;
-        setTimeout(() => {
-            this.scrollbarComponent.update();
 
-            this.listenToScrollbarEvents();
-        }, 500);
-    }
-    get items(): IDropdownItem[] {
-        return this._items;
-    }
+export class DropdownMultiselectComponent extends DropdownBaseComponent implements OnChanges {
+    @Input() displayAllItemsText: string;
+    @Output() selectionChanged = new EventEmitter<IDropdownItem[]>();
+    dropdownText: string;
+
     get filterActive(): boolean {
         return this.filterTextboxComponent && this.filterTextboxComponent.filterValue && this.filterTextboxComponent.filterValue != "";
     }
 
-    private listenToScrollbarEvents() {
-        $(this.scrollbarComponent.elementRef.nativeElement).scroll((e) => {
-            this.hideDimmersIfScrollIsAtBottomOrTop(e);
-        });
-    }
+    constructor(elementRef: ElementRef) {
+        super(elementRef);
 
-
-    constructor(private elementRef: ElementRef) {
-        this.expanded = false;
-        this.filterVisible = false;
-        this.scrollVisible = false;
-        this.filterPipe = new FilterPipe();
         this.displayAllItemsText = "Visa alla";
         this.selectAllItemText = "Välj alla";
         this.selectAllSelectedText = "Alla";
         this.dropdownText = "Välj";
     }
+
     ngOnChanges() {
         if (this.selectAllItemText) {
             this.selectAllItem = { displayName: this.selectAllItemText, displayNameWhenSelected: this.selectAllSelectedText, selected: false } as IDropdownItem;
@@ -216,34 +45,6 @@ export class DropdownMultiselectComponent implements OnChanges {
         this.filter = "";
         this.filterTextboxComponent.clear();
         this.preventCollapse = true;
-    }
-
-    ngOnInit() {
-
-    }
-
-    private hideDimmersIfScrollIsAtBottomOrTop(scrollEvent: JQueryEventObject) {
-        let scrollbar = $(scrollEvent.target);
-        let margintolerance = 20;
-
-        var scrollHeight = scrollEvent.target.scrollHeight - margintolerance;
-        var clientHeight = scrollEvent.target.clientHeight;
-        var scrollTop = scrollEvent.target.scrollTop;
-
-        if (clientHeight + scrollTop >= scrollHeight) {
-            scrollbar.next('.dropdown__dimmer--bottom').hide();
-        }
-        else {
-            scrollbar.next('.dropdown__dimmer--bottom').show();
-        }
-
-        if (scrollTop === 0) {
-            scrollbar.prev('.dropdown__dimmer--top').hide();
-        }
-        else {
-            scrollbar.prev('.dropdown__dimmer--top').show();
-        }
-
     }
 
     onItemCheckChanged(item: IDropdownItem) {
@@ -263,6 +64,11 @@ export class DropdownMultiselectComponent implements OnChanges {
             this.onItemCheckChanged(this.selectAllItem);
         }
     }
+
+    selectAllItems() {
+        this.selectItem(this.selectAllItem);
+    }
+
     selectItem(item: IDropdownItem) {
         if (!item)
             return;
@@ -277,11 +83,9 @@ export class DropdownMultiselectComponent implements OnChanges {
             this.selectAllItem.selected = this._items.filter(x => !x.selected).length === 0;
             this.selectionChanged.emit(this._items.filter(x => x.selected));
         }
-
-
         this.updateSelectedItemsCountText();
-
     }
+
     deselectItem(item: IDropdownItem) {
         if (!item)
             return;
@@ -295,7 +99,6 @@ export class DropdownMultiselectComponent implements OnChanges {
 
         this.selectAllItem.selected = false;
         this.updateSelectedItemsCountText();
-
     }
 
     private updateSelectedItemsCountText() {
@@ -309,53 +112,6 @@ export class DropdownMultiselectComponent implements OnChanges {
                 this.dropdownText = "Välj";
             else
                 this.dropdownText = selectedCount + " valda";
-
-        }
-    }
-
-    selectAllItems() {
-        this.selectItem(this.selectAllItem);
-    }
-
-    filterItems(filterValue: string) {
-        this.filter = filterValue;
-        this.updateScrolled();
-        //Scroll to top when filter is changed
-        $(".dropdown__menu__items").scrollTop(0);
-    }
-
-    updateScrolled() {
-
-        if (!this.items) {
-            this.scrollVisible = false;
-            return;
-        }
-
-        var visibleItemCount = this.filterPipe.transform(this.items, this.filter, ["displayName"]).length;
-
-        this.scrollVisible = visibleItemCount > this.scrollLimit;
-    }
-
-    toggleExpand(event: Event) {
-        if (this.preventCollapse) {
-            event.cancelBubble = true;
-            event.returnValue = false;
-            this.preventCollapse = false;
-            return;
-        }
-        let target = event.target || event.srcElement || event.currentTarget;
-        let element = $(target);
-        if (!element.is("input") && !element.is(".scroll-bar")) {
-            this.expanded = !this.expanded;
-        }
-    }
-
-    onDocumentClick(event: any) {
-
-        let target = event.target || event.srcElement || event.currentTarget;
-
-        if (!this.elementRef.nativeElement.contains(target)) {
-            this.expanded = false;
         }
     }
 
@@ -365,6 +121,5 @@ export class DropdownMultiselectComponent implements OnChanges {
 
     onMouseLeave(item: IDropdownItem) {
         item.marked = false;
-
     }
 }
