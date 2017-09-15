23efbe2eb3b9015e74e779a1b6ae1927163bc894
perakerberg <per.akerberg@evolvetechnology.se>
2017-09-15 07:27:34 +0200

    För att minska antalet varningar i loggen förbättrades pathen till karma-typescript preprocessorn

--- karma.conf.server.js
+++ karma.conf.server.js
@@ -47,7 +47,8 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': 'karma-typescript',
+      'tests/**/*.spec.ts': 'karma-typescript',
+      'component-package/**/*.ts': 'karma-typescript',
     },
 
     proxies: {
