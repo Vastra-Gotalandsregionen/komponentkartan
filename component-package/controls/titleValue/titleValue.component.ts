dd6797e5cb042482852ffd24af96fc5422d34787
Felicia Ringblom <felicia.ringblom@gmail.com>
2017-11-13 14:27:21 +0100

    #152270443 readonlyläge kort (#46)
    * Ta bort spacing på table rows och lägg till padding på table cells.
    Ny property slim till title-value komponenten som kan användas för att få tightare rader, t.ex vid readonly läge.
    
    * Använd slim property i exempel
    
    * Utöka list exemplet i demo-app
    
    * FIxa paddings och border sapcing för title-value layout för stora skrämar
    
    * Smabbare animering, byt ut disabled till readonly i exempel layout
    
    * Sätt maxhöjd för date- och month picker

--- component-package/controls/titleValue/titleValue.component.ts
+++ component-package/controls/titleValue/titleValue.component.ts
@@ -8,7 +8,12 @@ import { Input, Output, Component, HostBinding, ContentChild } from '@angular/co
 })
 export class TitleValueComponent {
     @HostBinding('class.title-value') private titleValueClass = true;
+    @HostBinding('class.slim-row')
+    public get isSlim(): boolean {
+        return this.slim;
+    }
     @Input() title: string;
+    @Input() slim: boolean;
     constructor() {
     }
 
