1ba5f6498d1feebb18e1a91dd8f58a1045de652f
John Pettersson <sigma.john.pettersson@outlook.com>
2017-11-27 17:00:55 +0100

    lagt till full width mode för card

--- component-package/controls/card/card.component.ts
+++ component-package/controls/card/card.component.ts
@@ -1,12 +1,24 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'vgr-card',
     moduleId: module.id,
     templateUrl: './card.component.html'
 })
-export class CardComponent {
-    @HostBinding('class.card') cardClass = true;
+export class CardComponent implements OnInit {
+    // @HostBinding('class.card') cardClass = true;
+    @Input() mode: CardMode;
     constructor() {
+        console.log(this.mode);
     }
+
+    ngOnInit() {
+        this.mode = this.mode === null || this.mode === undefined ? CardMode.fullWidth : this.mode;
+        console.log(this.mode);
+    }
+}
+
+enum CardMode {
+    fullWidth = 1,
+    twoCollumns = 2
 }
---
+++ demo-app/fake/full-width-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewChildren, QueryList, HostBinding } from '@angular/core';
+import { IDropdownItem } from '../../component-package/models/dropdownItem.model';
+
+@Component({
+  moduleId: module.id,
+  selector: 'vgr-full-width-card',
+  templateUrl: 'full-width-card.component.html'
+})
+export class FullWidthCardComponent {
+
+  constructor() { }
+
+}
