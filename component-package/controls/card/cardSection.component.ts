bc1eacc31118879f4e1c057d5259c025b5fb5a11
perakerberg <per.akerberg@evolvetechnology.se>
2017-11-28 15:36:31 +0100

    Ändringar för att få rätt avstånd mellan kort samt rättat en bugg i card section när man sätter expanded

--- component-package/controls/card/cardSection.component.ts
+++ component-package/controls/card/cardSection.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostBinding, Input, ElementRef } from '@angular/core';
+import { Component, HostBinding, Input, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
     selector: 'vgr-card-section',
     moduleId: module.id,
     templateUrl: './cardSection.component.html'
 })
-export class CardSectionComponent {
+export class CardSectionComponent implements AfterContentInit {
     @HostBinding('class.card-section') cardSectionClass = true;
     @HostBinding('class.card-section--expanded') private _expanded: boolean;
     @Input() @HostBinding('class.card-section--readonly') readonly: boolean;
@@ -13,10 +13,23 @@ export class CardSectionComponent {
     @Input() subtitle: string;
     @Input() set expanded(value: boolean) {
         this._expanded = value;
-        $(this.elementRef.nativeElement).children('.card-section__content').slideToggle(400);
+        this.setContentOpenOrClosed();
     }
     get expanded(): boolean {
         return this._expanded;
+    }
+
+    private setContentOpenOrClosed() {
+        if (this._expanded) {
+            $(this.elementRef.nativeElement).children('.card-section__content').slideDown(400);
+        } else {
+            $(this.elementRef.nativeElement).children('.card-section__content').slideUp(400);
+        }
+    }
+    ngAfterContentInit() {
+        setTimeout(() => {
+            this.setContentOpenOrClosed();
+        }, 10);
     }
     constructor(private elementRef: ElementRef) {
         this.readonly = true;
