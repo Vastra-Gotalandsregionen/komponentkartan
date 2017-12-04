6103e17958f263e9232af49509e3b149bf3e1ab4
perakerberg <per.akerberg@evolvetechnology.se>
2017-12-04 10:48:03 +0100

    Ändrat knapp till att använda click istället för mousedown

--- demo-app/loaderdemo/loaderdemo.component.ts
+++ demo-app/loaderdemo/loaderdemo.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'vgr-loader-demo',
-  templateUrl: 'loaderdemo.component.html'
+    moduleId: module.id,
+    selector: 'vgr-loader-demo',
+    templateUrl: 'loaderdemo.component.html'
 })
 export class LoaderDemoComponent {
-  title = 'app';
-  actionInProgress = false;
+    title = 'app';
+    actionInProgress = false;
 
-  get buttonText(): string {
-    if (!this.actionInProgress) {
-      return 'Start';
-    } else {
-      return 'Stop';
+    get buttonText(): string {
+        if (!this.actionInProgress) {
+            return 'Start';
+        } else {
+            return 'Stop';
+        }
     }
-  }
+
+    onClick(event: MouseEvent) {
+        console.log('Click! ');
+        this.actionInProgress = !this.actionInProgress
+    }
 }
