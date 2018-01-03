87efb9fe4322e5e66e0f50f0a4da5a14e59928c0
john <john.pettersson@sigma.se>
2018-01-03 14:01:05 +0100

    rensar

--- component-package/models/rowNotification.model.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationType } from './notificationType.model';
-import { NotificationIcon } from './notificationIcon.model';
-
-export interface RowNotification {
-    icon: NotificationIcon;
-    type: NotificationType;
-    message: string;
-    done: boolean;
-}
