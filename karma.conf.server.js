c72bc19da61a36b105ca27f73e056d9ff097a00b
Felicia Ringblom <felicia.ringblom@stenametall.se>
2018-01-04 08:39:20 +0100

    Lägg till kaqrma.conf.server.js igen

---
+++ karma.conf.server.js
@@ -0,0 +1,104 @@
+// Karma configuration
+// Generated on Thu Sep 14 2017 13:59:03 GMT+0200 (W. Europe Daylight Time)
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'karma-typescript'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'tests/**/*.spec.ts',
+      'index.ts',
+      'component-package/**/*.html',
+      'component-package/**/*.css',
+      'component-package/**/*.ts',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/systemjs/dist/system.js',
+      'node_modules/core-js/client/shim.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/fake-async-test.js',
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/intl/dist/Intl.js',
+      './systemjs.config.js',
+
+
+
+    ],
+
+
+    // list of files to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'index.ts': 'karma-typescript',
+      'component-package/**/*.ts': 'karma-typescript',
+      'tests/**/*.spec.ts': 'karma-typescript'
+    },
+
+    proxies: {
+      '/component-package/': '/base/component-package/'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['spec'],
+
+    specReporter: {
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressErrorSummary: false, // do not print error summary
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: false, // do not print information about skipped tests
+      showSpecTiming: true, // print the time elapsed for each spec
+      failFast: false // test would finish with error when a first fail occurs.
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: false,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
