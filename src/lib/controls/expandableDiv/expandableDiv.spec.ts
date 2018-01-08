44765a110ff9015e073c2285d151daea20c21e28
Felicia Ringblom <felicia.ringblom@stenametall.se>
2018-01-08 10:33:24 +0100

    Börja lägga till tester

---
+++ src/lib/controls/expandableDiv/expandableDiv.spec.ts
@@ -0,0 +1,14 @@
+import { ExpandableDivComponent } from './expandableDiv.component';
+import { ExpandableDivHeaderComponent } from './expandableDiv-header.component';
+import { ExpandableDivContentComponent } from './expandableDiv-content.component';
+
+describe('[ExpandableDivComponent]', () => {
+    const expandableDiv = new ExpandableDivComponent(null);
+
+    describe('When card contains two columns', () => {
+        beforeEach(() => {
+
+        });
+
+    });
+});
