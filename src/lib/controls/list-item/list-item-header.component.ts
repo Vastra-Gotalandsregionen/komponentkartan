585840a993c731b71f3e914471c445a5857532b6
john <john.pettersson@sigma.se>
2018-01-11 17:16:57 +0100

    fixat ctrl+pagDown +pgUp

--- src/lib/controls/list-item/list-item-header.component.ts
+++ src/lib/controls/list-item/list-item-header.component.ts
@@ -8,24 +8,34 @@ import { Component, HostBinding, HostListener, Input, Output, EventEmitter, Elem
 
 export class ListItemHeaderComponent {
     @HostBinding('class.list-item__header') listItemHeader = true;
-    @HostBinding("tabIndex") tabIndex = 0;
+    @HostBinding('tabIndex') tabIndex = 0;
     @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
     @Output() goToFirst: EventEmitter<any> = new EventEmitter();
     @Output() goToLast: EventEmitter<any> = new EventEmitter();
+    @Output() goUp: EventEmitter<any> = new EventEmitter();
+    @Output() goDown: EventEmitter<any> = new EventEmitter();
 
     @HostListener('keydown', ['$event'])
     toggleExpand(event: KeyboardEvent) {
         if (event.keyCode === 13 || event.keyCode === 32) {
-
             this.expandedChanged.emit(true);
+            event.preventDefault();
         }
-        if (event.keyCode === 36) { //Home
-
+        if (event.keyCode === 36) { // Home
             this.goToFirst.emit();
+            event.preventDefault();
         }
-        if (event.keyCode === 35) { //End
-            console.log('toggleExpand');
+        if (event.keyCode === 35) { // End
             this.goToLast.emit();
+            event.preventDefault();
+        }
+        if (event.altKey && event.keyCode === 33) { // Ctrl + PageUp
+            this.goUp.emit();
+            event.preventDefault();
+        }
+        if (event.altKey && event.keyCode === 34) { // PageDown
+            this.goDown.emit();
+            event.preventDefault();
         }
     }
 
