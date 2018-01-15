927fe22144d466ae82dc44f61a5de9b66f00e480
Felicia Ringblom <felicia.ringblom@stenametall.se>
2018-01-15 14:44:49 +0100

    Ändra så att value i en SelectableItem är av generisk typ

--- src/app/reactiveforms/reactiveforms.component.ts
+++ src/app/reactiveforms/reactiveforms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 import { ViewEncapsulation } from '@angular/core/src/metadata/view';
+import { SelectableItem } from '../../lib/index';
 
 @Component({
   selector: 'app-reactiveforms',
@@ -8,6 +9,10 @@ import { ViewEncapsulation } from '@angular/core/src/metadata/view';
   styleUrls: ['./reactiveforms.component.css']
 })
 export class ReactiveformsComponent implements OnInit {
+
+  radioOptions: SelectableItem<MyObject>[];
+  radioOptions2: SelectableItem<MyObject>[];
+
   userForm: FormGroup;
   validationMessages = {
     firstname: {
@@ -29,7 +34,13 @@ export class ReactiveformsComponent implements OnInit {
       'required': 'Detta skriver över default meddelandet för obligatoriska fält'
     }
   };
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {
+    this.radioOptions = [
+      { displayName: 'Ett', value: { id: '1', number: 1 } },
+      { displayName: 'Två', value: { id: '2', number: 2 } },
+      { displayName: 'Tre', value: { id: '3', number: 3 } }
+    ];
+  }
 
   ngOnInit() {
     this.createForm();
@@ -46,19 +57,11 @@ export class ReactiveformsComponent implements OnInit {
       favourite_pet: ['', Validators.required],
       interests: [['Koda'], Validators.required],
       check: [true, Validators.pattern('true')],
-      optional: [{ id: '2', displayName: 'Två' }],
+      optional: [this.radioOptions[2].value],
       monthpicker: ['', Validators.required],
       datepicker: ['', Validators.required],
       datepicker_preselected: [new Date(), Validators.required]
     });
-  }
-
-  onSubmit() {
-    console.log(this.userForm.controls.optional.value);
-  }
-
-  onChange(event: any) {
-    console.log('onChange', event);
   }
 }
 
@@ -70,4 +73,9 @@ function validateNumber(control: AbstractControl) {
     return null;
   }
   return { invalidNumber: true };
+}
+
+declare interface MyObject {
+  id: string;
+  number: number;
 }
