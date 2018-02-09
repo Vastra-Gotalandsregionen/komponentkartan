767de6fdd083aa28a28a1af7ecf80289cf5b0043
Felicia Ringblom <felicia.ringblom@gmail.com>
2018-02-09 18:34:42 +0100

    FOrtsatt implementation av notifieringar i meny

--- src/lib/controls/menu/menu-item.component.ts
+++ src/lib/controls/menu/menu-item.component.ts
@@ -1,19 +1,27 @@
-import { Input, Component, HostBinding } from '@angular/core';
+import { Input, Component } from '@angular/core';
 
 @Component({
     selector: 'vgr-menu-item',
     templateUrl: './menu-item.component.html'
 })
 export class MenuItemComponent {
-    // @HostBinding('class.menu-item') hasClass = true;
-
-
     @Input() text: string;
     @Input() link: string;
     @Input() disabled = false;
     @Input() disabledTooltip?: boolean;
-    @Input() notification?: number;
+    @Input() notification?: MenuItemNotification;
+
+    get notificationColor(): string {
+        return 'notification--' + this.notification.color;
+    }
 
     constructor() { }
 
+
+}
+
+class MenuItemNotification {
+    color: string;
+    text: string;
+    tooltip: string;
 }
