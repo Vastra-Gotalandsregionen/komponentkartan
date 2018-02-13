6f46f8d3695a38e548f975cd0589645fd205714c
Caroline Bornsjö <SE2321000131-P000000226420@vgregion.se>
2018-02-13 14:54:31 +0100

    tester för radiogroup

---
+++ src/lib/controls/radioGroup/radioGroup.component.spec.ts
@@ -0,0 +1,55 @@
+import { RadioGroupComponent, RadioGroupItem } from './radioGroup.component';
+import { SelectableItem } from '../../models/selectableItem.model'
+
+
+describe('[DropdownComponent]', () => {
+    let component: RadioGroupComponent;
+    let component2: RadioGroupComponent;
+    let source: SelectableItem<string>[] = [{ displayName: 'Kalle', value: '1', selected: true }, { displayName: 'Eva', value: '2' }, { displayName: 'Anna', value: '3' }];
+
+    beforeEach(() => {
+        component = new RadioGroupComponent(null, null);
+        component2 = new RadioGroupComponent(null, null);
+    });
+
+
+    describe('When two components has same source,', () => {
+        beforeEach(() => {
+            component.options = source;
+            component2.options = source;
+        });
+
+        it('component1 has three items', () => {
+            expect(component.radiogroupItems.length).toBe(3);
+        });
+        it('Kalle is selected on component1', () => {
+            expect(component.radiogroupItems[0].selected).toBe(true);
+        });
+        it('component2 has three items', () => {
+            expect(component2.radiogroupItems.length).toBe(3);
+        });
+        it('Kalle is selected on component2', () => {
+            expect(component2.radiogroupItems[0].selected).toBe(true);
+        });
+
+
+        describe('and component1 selects second option', () => {
+            beforeEach(() => {
+                component.optionClicked(component.radiogroupItems[1]);
+            });
+            it('Kalle is not selected on component1', () => {
+                expect(component.radiogroupItems[0].selected).toBe(false);
+            });
+            it('Eva is selected on component1', () => {
+                expect(component.radiogroupItems[1].selected).toBe(true);
+            });
+            it('Kalle is still selected on component2', () => {
+                expect(component2.radiogroupItems[0].selected).toBe(true);
+            });
+
+        });
+    });
+
+
+});
+
