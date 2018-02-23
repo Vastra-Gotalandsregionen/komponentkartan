296438b6642d9eaaaa422aadc70a727ab8fe24d4
john <john.pettersson@sigma.se>
2018-02-23 09:12:37 +0100

    hämtat från demo app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 import { KomponentkartanApplicationComponent } from './app.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { AppModule } from './app.module';
-
 describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [KomponentkartanApplicationComponent],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  });
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(KomponentkartanApplicationComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                KomponentkartanApplicationComponent
+            ],
+        }).compileComponents();
+    }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(KomponentkartanApplicationComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+    it(`should have as title 'app'`, async(() => {
+        const fixture = TestBed.createComponent(KomponentkartanApplicationComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('app');
+    }));
+    it('should render title in a h1 tag', async(() => {
+        const fixture = TestBed.createComponent(KomponentkartanApplicationComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    }));
 });
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 declare var require: any;
 
+
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
@@ -11,12 +12,12 @@ export class StartComponent implements OnInit {
   angularVersion: string;
 
   constructor() {
-    // const { dependencies: appVersion } = require('../../../package.json');
-    // this.currentVersion = appVersion['vgr-komponentkartan'];
-    // this.currentVersion = this.currentVersion.replace('^', '');
+    const { dependencies: appVersion } = require('../../../package.json');
+    this.currentVersion = appVersion['vgr-komponentkartan'];
+    this.currentVersion = this.currentVersion.replace('^', '');
 
-    // const { version: angularVersion } = require('../../../node_modules/@angular/core/package.json');
-    // this.angularVersion = angularVersion;
+    const { version: angularVersion } = require('../../../node_modules/@angular/core/package.json');
+    this.angularVersion = angularVersion;
   }
 
   ngOnInit() {
