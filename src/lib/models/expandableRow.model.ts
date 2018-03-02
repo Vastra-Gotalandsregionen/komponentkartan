c6aea0582306a22772f7ef447c2e7d2dbe384fe5
Caroline Bornsjö <SE2321000131-P000000226420@vgregion.se>
2018-03-02 14:18:21 +0100

    tog tillbaka en ändring...

--- src/lib/models/expandableRow.model.ts
+++ src/lib/models/expandableRow.model.ts
@@ -4,28 +4,24 @@ import { NotificationType } from './notificationType.model';
 export class ExpandableRow<TPreview, TFull> {
     previewObject: TPreview;
     fullObject: TFull;
-    private _notification: RowNotification; //the permanentNofication
+    private notification: RowNotification; //the permanentNofication
 
     constructor(previewObject: TPreview) {
         this.previewObject = previewObject;
     }
     setNotification(message: string, icon: string) {
-        this._notification = { icon: icon, message: message, type: NotificationType.Permanent } as RowNotification;
-    }
-
-    notification(): RowNotification {
-        return this._notification;
+        this.notification = { icon: icon, message: message, type: NotificationType.Permanent } as RowNotification;
     }
 
     notifyOnCollapse(message: string, icon: string, clearNotification: boolean = false) {
-        this._notification = { icon: icon, message: message, type: NotificationType.ShowOnCollapse, removeWhenDone: clearNotification } as RowNotification;
+        this.notification = { icon: icon, message: message, type: NotificationType.ShowOnCollapse, removeWhenDone: clearNotification } as RowNotification;
     }
 
     notifyOnRemove(message: string, icon: string) {
-        this._notification = { icon: icon, message: message, type: NotificationType.ShowOnRemove } as RowNotification;
+        this.notification = { icon: icon, message: message, type: NotificationType.ShowOnRemove } as RowNotification;
     }
 
     removeNotifiaction() {
-        this._notification = null;
+        this.notification = null;
     }
 }
