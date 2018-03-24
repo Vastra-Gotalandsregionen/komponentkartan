8750629b83a5c2c3dc9b60a49f27be79cdfdb09c
Felicia Ringblom <felicia.ringblom@gmail.com>
2018-03-24 10:28:26 +0100

    Rafaktorera så table extendar expandable-div

--- src/lib/controls/table/table.component.ts
+++ src/lib/controls/table/table.component.ts
@@ -1,83 +1,16 @@
-import { Input, Component, HostBinding, ContentChild, ElementRef, Output, EventEmitter } from '@angular/core';
-import { trigger, style, transition, animate, state } from '@angular/animations';
-
+import { Component, HostBinding, Output, EventEmitter } from '@angular/core';
+import { ExpandableDivComponent } from '../expandableDiv/expandableDiv.component';
 @Component({
     selector: 'vgr-table',
     moduleId: module.id,
-    templateUrl: './table.component.html',
-    animations: [
-        trigger('slideTableContent', [
-            state('collapsed', style({
-                overflow: 'hidden',
-                height: '0'
-            })),
-            state('expanded', style({
-                overflow: 'visible',
-                height: '*',
-                'margin-bottom': '25px'
-            })),
-            transition('expanded => collapsed',
-                animate('400ms ease-out')
-            ),
-            transition('collapsed => expanded',
-                animate('400ms ease-in')
-            ),
-        ])
-    ]
+    templateUrl: './table.component.html'
 })
-export class TableComponent {
-    @HostBinding('class.table--collapsed') private collapsed = true;
-    @HostBinding('class.table--expanded') private _expanded: boolean;
+export class TableComponent extends ExpandableDivComponent {
     @HostBinding('class.table') private tableClass = true;
-
     @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    @Input() set expanded(expandedValue: boolean) {
-        if (expandedValue && !this._expanded) {
-            this.expand();
-        } else if (!expandedValue && this._expanded) {
-            this.collapse();
-        }
-    }
-
-    get expanded(): boolean {
-        return this._expanded;
-    }
-
-    get chevron_class() {
-        return 'table-chevron '.concat(this.expanded ? 'expanded' : 'collapsed');
-    }
-
-    get stateName() {
-        return this.expanded ? 'expanded' : 'collapsed';
-    }
-
-    constructor(private elementRef: ElementRef) { }
-
-    animationDone($event) {
-        this.elementRef.nativeElement.style['overflow'] = 'visible';
-
-    }
-    animationStart($event) {
-        this.elementRef.nativeElement.style['overflow'] = 'hidden';
-    }
-
-    collapse() {
-        const expandedChanged = this._expanded;
-        this._expanded = false;
-        this.collapsed = true;
-        if (expandedChanged) {
-            this.expandedChanged.emit(this._expanded);
-        }
-    }
-
-    expand() {
-        const expandedChanged = !this._expanded;
-        this._expanded = true;
-        this.collapsed = false;
-        if (expandedChanged) {
-            this.expandedChanged.emit(this._expanded);
-        }
+    onExpandedChanged(expanded: boolean) {
+        this.expandedChanged.emit(expanded);
     }
 }
 
