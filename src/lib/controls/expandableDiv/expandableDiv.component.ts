42049e5700247646971d763a924d6204290b6cc8
sheriffjohn <john@terminal.se>
2018-03-26 13:29:07 +0200

    kommenterad bort tester

--- src/lib/controls/expandableDiv/expandableDiv.component.ts
+++ src/lib/controls/expandableDiv/expandableDiv.component.ts
@@ -1,84 +1,83 @@
-// import { Input, Component, HostBinding, ContentChild, ElementRef, Output, EventEmitter } from '@angular/core';
-// import { trigger, style, transition, animate, state } from '@angular/animations';
+import { Input, Component, HostBinding, ContentChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { trigger, style, transition, animate, state } from '@angular/animations';
 
-// @Component({
-//     selector: 'vgr-expandable-div',
-//     moduleId: module.id,
-//     templateUrl: './expandableDiv.component.html',
-//     animations: [
-//         trigger('slideExpandableContent', [
-//             state('collapsed', style({
-//                 overflow: 'hidden',
-//                 height: '0'
-//             })),
-//             state('expanded', style({
-//                 overflow: 'visible',
-//                 height: '*',
-//                 'margin-bottom': '25px'
-//             })),
-//             transition('expanded => collapsed',
-//                 animate('400ms ease-out')
-//             ),
-//             transition('collapsed => expanded',
-//                 animate('400ms ease-in')
-//             ),
-//         ])
-//     ]
-// })
-// export class ExpandableDivComponent {
-//     @HostBinding('class.expandable-div--collapsed') private collapsed = true;
-//     @HostBinding('class.expandable-div--expanded') private _expanded: boolean;
-//     @HostBinding('class.expandable-div') private expandableDivClass = true;
+@Component({
+    selector: 'vgr-expandable-div',
+    moduleId: module.id,
+    templateUrl: './expandableDiv.component.html',
+    animations: [
+        trigger('slideExpandableContent', [
+            state('collapsed', style({
+                overflow: 'hidden',
+                height: '0'
+            })),
+            state('expanded', style({
+                overflow: 'visible',
+                height: '*',
+            })),
+            transition('expanded => collapsed',
+                animate('400ms ease-out')
+            ),
+            transition('collapsed => expanded',
+                animate('400ms ease-in')
+            ),
+        ])
+    ]
+})
+export class ExpandableDivComponent {
+    @HostBinding('class.expandable-div--collapsed') private collapsed = true;
+    @HostBinding('class.expandable-div--expanded') private _expanded: boolean;
+    @HostBinding('class.expandable-div') private expandableDivClass = true;
 
-//     @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-//     @Input() set expanded(expandedValue: boolean) {
-//         if (expandedValue && !this._expanded) {
-//             this.expand();
-//         } else if (!expandedValue && this._expanded) {
-//             this.collapse();
-//         }
-//     }
+    @Input() set expanded(expandedValue: boolean) {
+        if (expandedValue && !this._expanded) {
+            this.expand();
+        } else if (!expandedValue && this._expanded) {
+            this.collapse();
+        }
+    }
 
-//     get expanded(): boolean {
-//         return this._expanded;
-//     }
+    get expanded(): boolean {
+        return this._expanded;
+    }
 
-//     get chevron_class() {
-//         return 'expandable-div-chevron '.concat(this.expanded ? 'expanded' : 'collapsed');
-//     }
+    get chevron_class() {
+        return 'expandable-div-chevron '.concat(this.expanded ? 'expanded' : 'collapsed');
+    }
 
-//     get stateName() {
-//         return this.expanded ? 'expanded' : 'collapsed';
-//     }
+    get stateName() {
+        return this.expanded ? 'expanded' : 'collapsed';
+    }
 
 
-//     constructor(private elementRef: ElementRef) { }
+    constructor(private elementRef: ElementRef) { }
 
-//     animationDone($event) {
-//         this.elementRef.nativeElement.style['overflow'] = 'visible';
+    animationDone($event) {
+        this.elementRef.nativeElement.style['overflow'] = 'visible';
 
-//     }
-//     animationStart($event) {
-//         this.elementRef.nativeElement.style['overflow'] = 'hidden';
-//     }
+    }
+    animationStart($event) {
+        this.elementRef.nativeElement.style['overflow'] = 'hidden';
+    }
 
-//     collapse() {
-//         const expandedChanged = this._expanded;
-//         this._expanded = false;
-//         this.collapsed = true;
-//         if (expandedChanged) {
-//             this.expandedChanged.emit(this._expanded);
-//         }
-//     }
+    collapse() {
+        const expandedChanged = this._expanded;
+        this._expanded = false;
+        this.collapsed = true;
+        if (expandedChanged) {
+            this.expandedChanged.emit(this._expanded);
+        }
+    }
 
-//     expand() {
-//         const expandedChanged = !this._expanded;
-//         this._expanded = true;
-//         this.collapsed = false;
-//         if (expandedChanged) {
-//             this.expandedChanged.emit(this._expanded);
-//         }
-//     }
-// }
+    expand() {
+        const expandedChanged = !this._expanded;
+        this._expanded = true;
+        this.collapsed = false;
+        if (expandedChanged) {
+            this.expandedChanged.emit(this._expanded);
+        }
+    }
+}
 
