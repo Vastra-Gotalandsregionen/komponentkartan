a55b15e11c68a97a58cb0a40d264f3d88b8bc5ed
Sofia Hejdenberg <sofia.hejdenberg@soprasteria.com>
2018-05-04 11:39:05 +0200

    WIP animeringar av items i lista

--- src/app/examples-listwithlists/examples-listwithlists.component.ts
+++ src/app/examples-listwithlists/examples-listwithlists.component.ts
@@ -71,9 +71,9 @@ export class ExamplesListwithlistsComponent {
   private mapToListItems(enheter: ExampleUnit[]) {
     this.listData = enheter.filter(x => !x.deleted).map(x => new ExpandableRow<ExampleUnit, any>(x));
     this.listData.forEach(element => {
-      if (this.getRandomInt(0, 5) === 2) {
-        element.setNotification('Meddelande om denna rad som ligger permanent', 'vgr-icon-exclamation');
-      }
+      // if (this.getRandomInt(0, 5) === 2) {
+      //   element.setNotification('Meddelande om denna rad som ligger permanent', 'vgr-icon-exclamation');
+      // }
     });
   }
 
