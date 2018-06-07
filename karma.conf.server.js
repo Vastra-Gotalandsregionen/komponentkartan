6f64b316654b9fee62c93cdf3362b4ff623daa74
sheriffjohn <john@terminal.se>
2018-06-07 17:03:56 +0200

    mer fix av karma config

--- karma.conf.server.js
+++ karma.conf.server.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +20,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      environment: 'dev'
+      environment: 'prod'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
