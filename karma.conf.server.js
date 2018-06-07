80ef62187f0ae6a502523139060cfcfd5aa15f03
sheriffjohn <john@terminal.se>
2018-06-07 17:09:35 +0200

    mer fix, ändra tillbaka  till dev env

--- karma.conf.server.js
+++ karma.conf.server.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      environment: 'prod'
+      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
