ffb818a4c3fc956b1c1dc3b1e6f168551c87b1cb
Caroline Bornsjö <SE2321000131-P000000226420@vgregion.se>
2018-06-19 10:28:17 +0200

    wip: Uppdaterat lite exempel

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { DropdownItem } from 'vgr-komponentkartan';
+import { Component, OnInit, ContentChild, ViewChild } from '@angular/core';
+import { DropdownItem, SearchResultComponent } from 'vgr-komponentkartan';
 
 @Component({
   selector: 'app-search-results',
@@ -11,10 +11,17 @@ export class SearchResultsComponent implements OnInit {
   items: any = this.getDemoItems(55);
   filteredItems: any;
   dropdownVisible = false;
+  dropdownVisible_e1 = false;
   filterBoxValue: string;
   searchDescription = null;
 
-  constructor() { }
+  @ViewChild(SearchResultComponent) searchResult: SearchResultComponent;
+
+
+  constructor() {
+
+
+  }
 
   private getDemoItems(numberOfItems: number): DropdownItem<string>[] {
     const items: DropdownItem<string>[] = [];
@@ -24,23 +31,33 @@ export class SearchResultsComponent implements OnInit {
     return items;
   }
 
-  filterSearch(searchText: string = '') {
+  filterSearch(event) {
+    const searchText = event.target.value;
+
     if (!searchText || searchText.length < 3) {
-      this.dropdownVisible = false;
+      this.dropdownVisible_e1 = false;
       return;
     }
     this.filteredItems = this.items.filter(item => item.displayName.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
     this.searchDescription = this.filteredItems.length + ' träffar i "VGR" KIV.';
-    this.dropdownVisible = true;
+    this.dropdownVisible_e1 = true;
   }
 
-  click(item) {
-    console.log(item);
+  // click(item) {
+  //   //console.log(item);
+  //   //this.filterBoxValue = item.value;
+  //   this.dropdownVisible = false;
+
+  // }
+
+  setResult(item) {
     this.filterBoxValue = item.value;
-    this.dropdownVisible = false;
+    this.dropdownVisible_e1 = false;
+    console.log(item.displayName);
   }
 
   ngOnInit() {
+    this.searchResult.itemClick.subscribe((item) => this.setResult(item));
   }
 
 }
