16a9fb9cc0b303770075e80704d36c7965799367
Arvid Sollenby <arvid.sollenby@gmail.com>
2018-08-14 15:17:38 +0200

    WCAG anpassning av VGR-table

--- projects/komponentkartan/src/lib/controls/table/table.component.ts
+++ projects/komponentkartan/src/lib/controls/table/table.component.ts
@@ -1,15 +1,41 @@
-import { Component, HostBinding, Output, EventEmitter } from '@angular/core';
+import { Component, HostBinding, Output, EventEmitter, HostListener } from '@angular/core';
 import { ExpandableDivComponent } from '../expandableDiv/expandableDiv.component';
+import { trigger, style, transition, animate, group, state, query } from '@angular/animations';
 @Component({
     selector: 'vgr-table',
-    templateUrl: './table.component.html'
+    templateUrl: './table.component.html',
+    animations: [
+        trigger('toggleExpandedState', [
+            transition(':enter', [
+                style({ height: '0'}),
+                animate('0.4s ease', style({ height: '*' })),
+            ]),
+            transition(':leave', [
+                style({ height: '*'}),
+                animate('0.4s ease', style({ height: '0' })),
+            ]),
+        ])
+    ]
 })
 export class TableComponent extends ExpandableDivComponent {
     @HostBinding('class') tableClass = 'table';
 
     @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+    rowExpanded = false;
+    @HostListener('keydown', ['$event']) toggleRow(event: KeyboardEvent) {
+        if (event.keyCode === 13 || event.keyCode === 32) { // enter & space
+            event.preventDefault();
+            event.stopPropagation();
+            if (!this.rowExpanded) {
+                this.expand();
+            } else {
+                this.collapse();
+            }
+        }
+    }
 
     onExpandedChanged(expanded: boolean) {
+        this.rowExpanded = expanded;
         this.expandedChanged.emit(expanded);
     }
 }
