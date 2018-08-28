90f392111c80ac81715eeac5c365ae892cd8b1ba
Joel Erwing Bjelkholm <joel.bjelkholm@vgregion.se>
2018-08-28 19:01:24 +0200

    Påbörjat refakt av action panel.

--- projects/komponentkartan/src/lib/controls/action-panel/action-panel.component.ts
+++ projects/komponentkartan/src/lib/controls/action-panel/action-panel.component.ts
@@ -1,7 +1,5 @@
-import { Component, HostListener, HostBinding, OnInit, Input, Output, EventEmitter, ElementRef, ChangeDetectorRef, AfterContentInit } from '@angular/core';
+import { Component, HostBinding, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
-import { NotificationType } from '../../models/notificationType.model';
-import { RowNotification } from '../../models/rowNotification.model';
 
 @Component({
   templateUrl: './action-panel.component.html',
@@ -21,23 +19,26 @@ import { RowNotification } from '../../models/rowNotification.model';
     )
   ]
 })
-export class ActionPanelComponent implements OnInit, AfterContentInit {
-
-  readonly showNotificationDurationMs = 1500;
-  private actualContentHeight: string;
-  private pageHeaderHeight: number;
-
-  @HostBinding('class.action-panel') isContainer = true;
-  @HostBinding('class.action-panel--collapsed') collapsed = true;
-  @HostBinding('class.action-panel--expanded') _expanded: boolean;
-  @HostBinding('class.action-panel--deleted') deleted: boolean;
-  @HostBinding('class.action-panel--notification-visible') notificationVisible: boolean;
-  @HostBinding('class.action-panel--not-interactable') notInteractable: boolean;
-
-  @Input() showCloseButton: boolean;
+export class ActionPanelComponent {
 
   @Input() title: string;
+  @Input() showCloseButton = true;
   @Input() expansionSpeed: 'slow' | 'normal' | 'fast' | 'noanimation';
+  @Input() set expanded(value: boolean) {
+    if (value && !this._expanded) {
+      this.expand();
+    } else if (!value && this._expanded) {
+      this.collapse();
+    }
+  }
+  get expanded(): boolean {
+    return this._expanded;
+  }
+  readonly showNotificationDurationMs = 1500;
+  collapsed = true;
+  private pageHeaderHeight = 0;
+  private _expanded: boolean;
+
   get animationDelayMs(): number {
     if (this.expansionSpeed === 'slow') {
       return 1000;
@@ -49,6 +50,7 @@ export class ActionPanelComponent implements OnInit, AfterContentInit {
       return 600;
     }
   }
+
   @HostBinding('class.action-panel--slow') get slow() {
     return this.expansionSpeed === 'slow';
   }
@@ -59,108 +61,34 @@ export class ActionPanelComponent implements OnInit, AfterContentInit {
     return this.expansionSpeed === 'noanimation';
   }
 
-  @Input() set expanded(expandedValue: boolean) {
-    // this.elementRef.nativeElement.style.opacity = 0;
+  @Output() expandedChanged = new EventEmitter<boolean>();
 
-    if (expandedValue && !this._expanded) {
-      this.expand();
-    } else if (!expandedValue && this._expanded) {
-      this.collapse();
-    }
-  }
-  get expanded(): boolean {
-    return this._expanded;
-  }
-  @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() notificationChanged: EventEmitter<RowNotification> = new EventEmitter<RowNotification>();
-
-  private _notification: RowNotification;
-  @Input() set notification(value: RowNotification) {
-    this._notification = value;
-    if (value) {
-      if (value.type === NotificationType.ShowOnCollapse) {
-        this.collapse(value.type);
-        this.changeDetecor.detectChanges();
-      } else if (value.type === NotificationType.ShowOnRemove) {
-        this.collapse(value.type);
-        this.changeDetecor.detectChanges();
-      } else if (value.type === NotificationType.Permanent) {
-        this.showNotification();
-      }
-    }
-    this.notificationChanged.emit(value);
-  }
-  get notification(): RowNotification {
-    return this._notification;
-  }
-
-  constructor(private elementRef: ElementRef, private changeDetecor: ChangeDetectorRef) {
-    this.pageHeaderHeight = 0;
-  }
-
-  ngAfterContentInit() {
-    this.updateActualContentHeight();
-  }
-
-  private updateActualContentHeight() {
-    this.actualContentHeight = this.elementRef.nativeElement.scrollHeight + 'px';
-  }
+  constructor(private elementRef: ElementRef) { }
 
   public setPageHeaderHeight(height: number) {
     this.pageHeaderHeight = height;
-    this.elementRef.nativeElement.style.top = height + 'px';
-  }
-
-  ngOnInit() {
-    if (this.notification && this.notification.type === NotificationType.Permanent) {
-      this.showNotification();
-    }
-  }
-
-  showNotification() {
-    this.notificationVisible = true;
+    this.elementRef.nativeElement.children[0].style.top = height + 'px';
   }
 
   private expand() {
-    if (this.deleted || this.notInteractable) {
-      return;
-    }
-    this.updateActualContentHeight();
-    this.elementRef.nativeElement.style.height = this.actualContentHeight;
+    this.elementRef.nativeElement.children[0].style.height = this.elementRef.nativeElement.children[0].scrollHeight + 'px';
     this._expanded = true;
     this.collapsed = false;
-    this.expandedChanged.emit(this._expanded);
+    this.expandedChanged.emit(this.expanded);
     setTimeout(() => {
-      this.elementRef.nativeElement.style.height = 'auto';
-      this.elementRef.nativeElement.style.overflow = 'visible';
+      this.elementRef.nativeElement.children[0].style.height = 'auto';
+      this.elementRef.nativeElement.children[0].style.overflow = 'visible';
     }, this.animationDelayMs);
   }
 
-  private collapse(collapsingNotification?: NotificationType) {
-    this.updateActualContentHeight();
-    this.elementRef.nativeElement.style.height = this.actualContentHeight;
+  private collapse() {
+    this.elementRef.nativeElement.children[0].style.height = this.elementRef.nativeElement.children[0].scrollHeight + 'px';
     this._expanded = false;
     this.collapsed = true;
     this.expandedChanged.emit(false);
     setTimeout(() => {
-      this.elementRef.nativeElement.style.height = '0px';
-      this.elementRef.nativeElement.style.overflow = 'hidden';
+      this.elementRef.nativeElement.children[0].style.height = '0px';
+      this.elementRef.nativeElement.children[0].style.overflow = 'hidden';
     }, 50);
-  }
-
-  private processNotification(notificationType: NotificationType, callback: Function): void {
-    if (notificationType === NotificationType.ShowOnCollapse) {
-      this.processShowOnCollapseNotification(callback);
-    } else if (notificationType === NotificationType.ShowOnRemove) {
-      this.processShowOnRemoveNotification(callback);
-    }
-  }
-
-  private processShowOnCollapseNotification(callback: Function) {
-    this.notificationVisible = true;
-  }
-
-  private processShowOnRemoveNotification(callback: Function) {
-    this.notificationVisible = true;
   }
 }
