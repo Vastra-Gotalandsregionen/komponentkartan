deb3940664e87b2c06aee502b5ad42585d20d637
Joel Erwing Bjelkholm <joel.bjelkholm@vgregion.se>
2018-10-19 18:49:16 +0200

    Lagt till metod för att fokusera filter textbox.

--- projects/komponentkartan/src/lib/controls/filterTextbox/filterTextbox.component.ts
+++ projects/komponentkartan/src/lib/controls/filterTextbox/filterTextbox.component.ts
@@ -1,23 +1,31 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-    selector: 'vgr-filter-textbox',
-    templateUrl: './filterTextbox.component.html'
+  selector: 'vgr-filter-textbox',
+  templateUrl: './filterTextbox.component.html'
 })
 export class FilterTextboxComponent {
-    public hasFocus: boolean;
-    private _value: string;
+  @Input() get value() {
+    return this._value;
+  }
+  set value(newValue: string) {
+    this._value = newValue;
+    this.valueChanged.emit(newValue);
+  }
 
-    @Output() valueChanged = new EventEmitter<string>();
-    @Input() get value() {
-        return this._value;
-    }
-    set value(newValue: string) {
-        this._value = newValue;
-        this.valueChanged.emit(newValue);
-    }
-    clear() {
-        this._value = '';
-        this.valueChanged.emit('');
-    }
+  @Output() valueChanged = new EventEmitter<string>();
+
+  @ViewChild('filterTextbox') filterTextbox: ElementRef;
+
+  hasFocus: boolean;
+  private _value: string;
+
+  clear() {
+    this._value = '';
+    this.valueChanged.emit('');
+  }
+
+  focus() {
+    this.filterTextbox.nativeElement.focus();
+  }
 }
