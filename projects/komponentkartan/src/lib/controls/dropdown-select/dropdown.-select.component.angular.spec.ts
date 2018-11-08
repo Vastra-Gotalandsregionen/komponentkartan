4c5fee9c0b0b35b297421acb2ac0abcf6f209082
Joel Erwing Bjelkholm <joel.bjelkholm@vgregion.se>
2018-11-08 18:14:55 +0100

    Fixat test för dropdown select.

--- projects/komponentkartan/src/lib/controls/dropdown-select/dropdown.-select.component.angular.spec.ts
+++ projects/komponentkartan/src/lib/controls/dropdown-select/dropdown.-select.component.angular.spec.ts
@@ -1,6 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
 
 import { DropdownSelectComponent } from './dropdown-select.component';
+import { ButtonComponent } from '../button/button.component';
+import { InputComponent } from '../input/input.component';
+import { FilterTextboxComponent } from '../filterTextbox/filterTextbox.component';
+import { TruncatePipe } from '../../pipes/truncatePipe';
+
+@Component({
+  selector: 'vgr-filter-textbox',
+  template: '<div>Mock</div>'
+})
+class MockFilterTextboxComponent {}
+
+@Component({
+  selector: 'perfect-scrollbar',
+  template: '<ng-content></ng-content>'
+})
+class MockPerfectScrollbarComponent {
+  @Input() config;
+}
 
 describe('[DropdownSelectComponent - Angular]', () => {
   let component: DropdownSelectComponent;
@@ -8,7 +27,13 @@ describe('[DropdownSelectComponent - Angular]', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DropdownSelectComponent ]
+      declarations: [
+        DropdownSelectComponent,
+        ButtonComponent,
+        MockFilterTextboxComponent,
+        MockPerfectScrollbarComponent,
+        TruncatePipe
+      ]
     })
     .compileComponents();
   }));
