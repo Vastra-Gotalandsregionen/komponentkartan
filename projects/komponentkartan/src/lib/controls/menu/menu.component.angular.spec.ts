17fc8c8785589d375d4bed9f4f05e4ff75a8dfad
Fredrik Aronsson <fredrik.aronsson@vgregion.se>
2019-04-16 11:25:18 +0200

    Designfix samt byte till fa-ikoner av header-meny och huvud-meny

--- projects/komponentkartan/src/lib/controls/menu/menu.component.angular.spec.ts
+++ projects/komponentkartan/src/lib/controls/menu/menu.component.angular.spec.ts
@@ -6,6 +6,8 @@ import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/t
 import { CommonModule } from '@angular/common';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import { IconComponent } from '../icon/icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
     selector: 'vgr-test',
@@ -31,12 +33,13 @@ describe('[MenuComponent]', () => {
 
     beforeEach((done) => {
         TestBed.configureTestingModule({
-            declarations: [TestMenuComponent, MenuComponent, MenuItemComponent, SubmenuComponent],
+            declarations: [TestMenuComponent, MenuComponent, MenuItemComponent, SubmenuComponent, IconComponent],
             imports: [CommonModule,
                 BrowserAnimationsModule,
                 BrowserDynamicTestingModule,
                 NoopAnimationsModule,
-                RouterTestingModule.withRoutes([])
+                RouterTestingModule.withRoutes([]),
+                FontAwesomeModule
             ],
 
 
