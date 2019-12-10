11993acd00af3a44d249d00885f237a3b762c21f
Joel Erwing Bjelkholm <joel.bjelkholm@vgregion.se>
2019-12-10 14:51:27 +0100

    Refakt av expandable div.

--- projects/komponentkartan/src/lib/controls/expandableDiv/expandableDiv.component.ts
+++ projects/komponentkartan/src/lib/controls/expandableDiv/expandableDiv.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, HostBinding, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Input, Component, ElementRef, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { trigger, style, transition, animate, state, AnimationEvent } from '@angular/animations';
 
 @Component({
@@ -14,35 +14,33 @@ import { trigger, style, transition, animate, state, AnimationEvent } from '@ang
         overflow: 'visible',
         height: '*',
       })),
-      state('void', style({
-        overflow: 'hidden',
-        height: '0'
-      })),
       transition('expanded => collapsed',
         animate('400ms ease-out')
       ),
       transition('collapsed => expanded',
         animate('400ms ease-in')
-      ),
-      transition('void => expanded',
-        animate('400ms ease-in')
       )
     ])
   ]
 })
-export class ExpandableDivComponent {
-  @HostBinding('class.expandable-div--collapsed') collapsed = true;
-  @HostBinding('class.expandable-div--expanded') _expanded = false;
-  @HostBinding('class.expandable-div') expandableDivClass = true;
+export class ExpandableDivComponent implements OnChanges {
+  @Input() expanded = false;
   @Output() expandedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('content', { static: true }) content: ElementRef;
+  stateName = 'collapsed';
 
-  @HostListener('keydown', ['$event']) toggleRow(event: KeyboardEvent) {
-    const target = <HTMLDivElement>event.target;
-    const targetClass = target.className;
-    if (targetClass === 'expandable-div-header' && (event.keyCode === 13 || event.keyCode === 32)) { // enter & space
+  ngOnChanges(changes: SimpleChanges) {
+    const expandedChange = changes['expanded'];
+    if (expandedChange) {
+      this.stateName = this.expanded ? 'expanded' : 'collapsed';
+    }
+  }
+
+  onKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === 'Spacebar' || event.key === ' ') {
       event.preventDefault();
       event.stopPropagation();
-      if (!this._expanded) {
+      if (!this.expanded) {
         this.expand();
       } else {
         this.collapse();
@@ -50,57 +48,38 @@ export class ExpandableDivComponent {
     }
   }
 
-  @Input() set expanded(expandedValue: boolean) {
-    if (expandedValue && !this._expanded) {
-      this.expand();
-    } else if (!expandedValue && this._expanded) {
-      this.collapse();
-    }
-  }
-
-  get expanded(): boolean {
-    return this._expanded;
-  }
-
-  get chevron_class() {
-    return this.expanded ? 'expanded' : 'collapsed';
-  }
-
-  get stateName() {
-    return this.expanded ? 'expanded' : 'collapsed';
-  }
-
-
-  constructor(private elementRef: ElementRef) { }
+  constructor() { }
 
   animationStart(event: AnimationEvent) {
     if (event.toState === 'collapsed') {
-      this.elementRef.nativeElement.style['overflow'] = 'hidden';
+      this.content.nativeElement.style['overflow'] = 'hidden';
     }
   }
 
   animationDone(event: AnimationEvent) {
     if (event.toState === 'expanded') {
-      this.elementRef.nativeElement.style['overflow'] = 'visible';
+      this.content.nativeElement.style['overflow'] = 'visible';
+    }
+  }
+
+  toggleExpanded() {
+    if (this.expanded) {
+      this.collapse();
+    } else {
+      this.expand();
     }
   }
 
   collapse() {
-    const expandedChanged = this._expanded;
-    this._expanded = false;
-    this.collapsed = true;
-    if (expandedChanged) {
-      this.expandedChanged.emit(this._expanded);
-    }
+    this.expanded = false;
+    this.stateName = 'collapsed';
+    this.expandedChanged.emit(this.expanded);
   }
 
   expand() {
-    const expandedChanged = !this._expanded;
-    this._expanded = true;
-    this.collapsed = false;
-    if (expandedChanged) {
-      this.expandedChanged.emit(this._expanded);
-    }
+    this.expanded = true;
+    this.stateName = 'expanded';
+    this.expandedChanged.emit(this.expanded);
   }
 }
 
