88a8986c85a236cf682ca631086f3da5b5a9a448
Synnöve Lysö <synnove.lyso@elicit.se>
2020-03-09 13:35:50 +0100

    Exempel på readonly och kodstäd.

--- src/app/textareaField/textareafield.component.ts
+++ src/app/textareaField/textareafield.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TextareaComponent } from '../../../projects/komponentkartan/src/public_api';
 
