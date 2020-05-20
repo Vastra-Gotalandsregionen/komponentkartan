82ca061d7847970a4a6712cf914b7b07d0a5247b
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-05-20 15:30:21 +0200

    rensa kod

--- src/app/reactiveforms-example/reactiveforms-example.component.ts
+++ src/app/reactiveforms-example/reactiveforms-example.component.ts
@@ -166,25 +166,8 @@ export class ReactiveformsexampleComponent implements OnInit {
 
     handleLockChanged(state) {
         this.locked = !this.locked;
-        /*
-        this.updateOnBlurForm = new FormGroup({
-            firstname: new FormControl('', { validators: [Validators.required, Validators.minLength(2)] }),
-            lastname: new FormControl('', { validators: [Validators.required, Validators.minLength(2)] }),
-            age: new FormControl('', { validators: [Validators.required, Validators.min(18), Validators.max(120), validateNumber] }),
-            email: new FormControl('', { validators: [Validators.required, Validators.email] }),
-            salary: new FormControl('', { validators: [Validators.required, validateNumber] }),
-            favourite_pet: new FormControl(null, { validators: [Validators.required] }),
-            interests: new FormControl(null, { validators: [Validators.required, Validators.pattern('Koda')] }),
-            check: new FormControl(true, { validators: [Validators.pattern('true')] }),
-            optional: new FormControl(1),
-            monthpicker: new FormControl('', { validators: [Validators.required] }),
-            datepicker: new FormControl('', { validators: [Validators.required] }),
-            datepicker_preselected: new FormControl(new Date(), { validators: [Validators.required] }),
-            textarea: new FormControl('', { validators: [Validators.required] })
-        }, { updateOn: 'blur' });
 
-        */
-        Object.keys(this.updateOnBlurForm.controls).forEach(key => {
+        Object.keys(this.updateOnBlurForm.controls).map(key => {
             if (state) {
                 this.updateOnBlurForm.controls[key].enable();
             } else {
