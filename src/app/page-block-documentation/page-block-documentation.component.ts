e7082e89d78fbd667b3001105c6107e350e85396
Henning Klasén <henning@fastmail.com>
2020-05-22 10:52:13 +0200

    Relativ positon av vgr-page-block samt vgr-action-panel
    * Update to latest angular 9.1.7
    
    * Update angular.json
    
    * Add an example of action-panel with error notification
    
    * Set position relative to add compatibility with vgr-notification
    
    * Update documentation and examples for page block, re vgr-notification
    
    * Set page block to be a relative position
    
    Co-authored-by: Henning Klasén <henning.klasen@vgregion.se>

--- src/app/page-block-documentation/page-block-documentation.component.ts
+++ src/app/page-block-documentation/page-block-documentation.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HtmlEncodeService } from '../html-encode.service';
 
 @Component({
   selector: 'app-page-block-documentation',
   templateUrl: './page-block-documentation.component.html'
 })
-export class PageBlockDocumentationComponent { }
+export class PageBlockDocumentationComponent {
+  exampleCodeMarkup: string;
+  exampleCode =
+`<vgr-page-block>
+  <vgr-notification type="error">
+    Något gick fel.
+  </vgr-notification>
+</vgr-page-block>`;
+
+  constructor(htmlEncoder: HtmlEncodeService) {
+    this.exampleCodeMarkup =
+      htmlEncoder.prepareHighlightedSection(this.exampleCode, 'HTML');
+  }
+}
