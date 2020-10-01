1f9704615db7679b07e760606cc60edc4ea3c31d
Henning Klasén <henning.klasen@vgregion.se>
2020-10-01 11:17:11 +0200

    Add delay before focus in IE11.

--- projects/komponentkartan/src/lib/controls/filter-tag/filter-tag.component.ts
+++ projects/komponentkartan/src/lib/controls/filter-tag/filter-tag.component.ts
@@ -41,7 +41,22 @@ export class FilterTagComponent implements AfterViewInit {
   }
 
   focus() {
-    this.filtertag.nativeElement.focus();
+    // TODO: Remove this as soon as IE11
+    // support is dropped.
+    //
+    // focus() does not work in IE11 unless
+    // set after a short delay. Wrap in if
+    // to not do this in better browsers.
+    const re = /Trident\/7/;
+    const isIE11 = re.test(window.navigator.userAgent);
+
+    if (isIE11) {
+      setTimeout(() => {
+        this.filtertag.nativeElement.focus();
+      }, 5);
+    } else {
+      this.filtertag.nativeElement.focus();
+    }
   }
 
   emitRemove() {
