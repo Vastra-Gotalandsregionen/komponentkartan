c6c0a0fca7649d78a3e00e83dddf9a961d3ef8ac
Arvid Sollenby <arvid.sollenby@gmail.com>
2020-10-29 08:23:55 +0100

    uppdaterat några tester

--- projects/komponentkartan/src/lib/controls/list/list.service.spec.ts
+++ projects/komponentkartan/src/lib/controls/list/list.service.spec.ts
@@ -6,7 +6,7 @@ describe('ListService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ListService = TestBed.get(ListService);
+    const service: ListService = TestBed.inject(ListService);
     expect(service).toBeTruthy();
   });
 });
--- projects/komponentkartan/src/lib/services/page-header-height.service.spec.ts
+++ projects/komponentkartan/src/lib/services/page-header-height.service.spec.ts
@@ -6,7 +6,7 @@ describe('PageHeaderHeightService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: PageHeaderHeightService = TestBed.get(PageHeaderHeightService);
+    const service: PageHeaderHeightService = TestBed.inject(PageHeaderHeightService);
     expect(service).toBeTruthy();
   });
 });
