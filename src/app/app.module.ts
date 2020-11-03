a5bad987d39f42de8b43dd6e935b900dbba78a4f
Arvid Sollenby <arvid.sollenby@gmail.com>
2020-11-03 09:51:15 +0100

    Rensat bort deprecated komponenter.
    och uppdaterat vgr-list till vgr-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,10 +29,8 @@ import { CheckboxComponent } from './checkbox/checkbox.component';
 import { ColoursComponent } from './colours/colours.component';
 import { FormattemplateComponent } from './formattemplate/formattemplate.component';
 import { HeadersComponent } from './headers/headers.component';
-import { ListsComponent } from './lists/lists.component';
 import { LockbuttonComponent } from './lockbutton/lockbutton.component';
 import { ClosebuttonComponent } from './closebutton/closebutton.component';
-
 import { PanelsComponent } from './panels/panels.component';
 import { RadiobuttonsComponent } from './radiobuttons/radiobuttons.component';
 import { TextbuttonsComponent } from './textbuttons/textbuttons.component';
@@ -41,15 +39,9 @@ import { BacktotopComponent } from './backtotop/backtotop.component';
 import { LoaderComponent } from './loader/loader.component';
 import { StartComponent } from './start/start.component';
 import { ThemingComponent } from './theming/theming.component';
-import { ListexampleComponent } from './lists/examples/withnotification/listexample.component';
-import { ListexamplewithrownotificationComponent } from './lists/examples/withrownotifications/listexamplewithrownotification.component';
-import { ListcodeexampleComponent } from './lists/examples/simple/listcodeexample.component';
-import { ListwithpaginationcodeexampleComponent } from './lists/examples/withpagination/listwithpaginationcodeexample.component';
-import { ListExampleWithActionButtonsComponent } from './lists/examples/withactionbuttons/listexamplewithactionbuttons.component';
 import { ReactiveformsexampleComponent } from './reactiveforms-example/reactiveforms-example.component';
 import { ComponentDocsComponent } from './component-docs/component-docs.component';
 import { TitleValueComponent } from './title-value/title-value.component';
-import { FiltertexboxComponent } from './filtertexbox/filtertexbox.component';
 import { PageStructureComponent } from './page-structure/page-structure.component';
 import { CardComponent } from './card/card.component';
 import { HtmlEncodeService } from './html-encode.service';
@@ -58,7 +50,6 @@ import { MenuComponent } from './menu/menu.component';
 import { ExpandableDivComponent } from './expandable-div/expandable-div.component';
 import { ActionPanelsComponent } from './actionpanels/actionpanels.component';
 import { FilterTagComponent } from './filter-tag/filter-tag.component';
-import { ListexamplewithexpandabledivComponent } from './lists/examples/withexpandablediv/listexamplewithexpandablediv.component';
 import { TextareafieldComponent } from './textareaField/textareafield.component';
 import { IconsComponent } from './icons/icons.component';
 import { TableComponent } from './table/table.component';
@@ -66,13 +57,10 @@ import { RingWithTextComponent } from './ring-with-text/ring-with-text.component
 import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { PageBlockDocumentationComponent } from './page-block-documentation/page-block-documentation.component';
-import { ExamplesListwithpageblockComponent } from './examples-listwithpageblock/examples-listwithpageblock.component';
 import { PageHeaderDocumentationComponent } from './page-header-documentation/page-header-documentation.component';
 import { DropdownSelectDocumentationComponent } from './dropdown-select-documentation/dropdown-select-documentation.component';
 import { PaginationDocumentationComponent } from './pagination-documentation/pagination-documentation.component';
 import { ModaldialogComponent } from './modaldialog/modaldialog.component';
-import { ExamplesListwithcardsComponent } from './examples-listwithcards/examples-listwithcards.component';
-import { UnitFilterPipe } from './examples-listwithcards/unitfilterpipe';
 import { GridDocumentationComponent } from './grid-documentation/grid-documentation.component';
 import { NotificationDocumentationComponent} from './notification-documentation/notification-documentation.component';
 
@@ -87,7 +75,6 @@ import { NotificationDocumentationComponent} from './notification-documentation/
         ColoursComponent,
         FormattemplateComponent,
         HeadersComponent,
-        ListsComponent,
         LockbuttonComponent,
         PanelsComponent,
         RadiobuttonsComponent,
@@ -95,21 +82,14 @@ import { NotificationDocumentationComponent} from './notification-documentation/
         LoaderComponent,
         StartComponent,
         ThemingComponent,
-        ListexampleComponent,
-        ListexamplewithrownotificationComponent,
         ComponentDocsComponent,
         TitleValueComponent,
-        FiltertexboxComponent,
         PageStructureComponent,
         CardComponent,
         SizesComponent,
         MenuComponent,
-        ListcodeexampleComponent,
-        ListwithpaginationcodeexampleComponent,
-        ListExampleWithActionButtonsComponent,
         ReactiveformsexampleComponent,
         ExpandableDivComponent,
-        ListexamplewithexpandabledivComponent,
         IconsComponent,
         ActionPanelsComponent,
         ClosebuttonComponent,
@@ -121,12 +101,9 @@ import { NotificationDocumentationComponent} from './notification-documentation/
         SearchResultsComponent,
         PageBlockDocumentationComponent,
         PageHeaderDocumentationComponent,
-        ExamplesListwithpageblockComponent,
-        ExamplesListwithcardsComponent,
         DropdownSelectDocumentationComponent,
         PaginationDocumentationComponent,
         ModaldialogComponent,
-        UnitFilterPipe,
         GridDocumentationComponent,
         NotificationDocumentationComponent
     ],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -9,7 +9,6 @@ import { ColoursComponent } from './colours/colours.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { FormattemplateComponent } from './formattemplate/formattemplate.component';
 import { HeadersComponent } from './headers/headers.component';
-import { ListsComponent } from './lists/lists.component';
 import { LoaderComponent } from './loader/loader.component';
 import { LockbuttonComponent } from './lockbutton/lockbutton.component';
 import { ClosebuttonComponent } from './closebutton/closebutton.component';
@@ -21,20 +20,12 @@ import { BacktotopComponent } from './backtotop/backtotop.component';
 import { StartComponent } from './start/start.component';
 import { ThemingComponent } from './theming/theming.component';
 import { TitleValueComponent } from './title-value/title-value.component';
-import { FiltertexboxComponent } from './filtertexbox/filtertexbox.component';
 import { PageStructureComponent } from './page-structure/page-structure.component';
 import { CardComponent } from './card/card.component';
 import { SizesComponent } from './sizes/sizes.component';
 import { MenuComponent } from './menu/menu.component';
-import { ListexampleComponent } from './lists/examples/withnotification/listexample.component';
-import { ListexamplewithrownotificationComponent } from './lists/examples/withrownotifications/listexamplewithrownotification.component';
-import { ListcodeexampleComponent } from './lists/examples/simple/listcodeexample.component';
-import { ListwithpaginationcodeexampleComponent } from './lists/examples/withpagination/listwithpaginationcodeexample.component';
-import { ListExampleWithActionButtonsComponent } from './lists/examples/withactionbuttons/listexamplewithactionbuttons.component';
 import { ReactiveformsexampleComponent } from './reactiveforms-example/reactiveforms-example.component';
 import { ExpandableDivComponent } from './expandable-div/expandable-div.component';
-import { ListexamplewithexpandabledivComponent } from './lists/examples/withexpandablediv/listexamplewithexpandablediv.component';
-import { ExamplesListwithpageblockComponent } from './examples-listwithpageblock/examples-listwithpageblock.component';
 import { TableComponent } from './table/table.component';
 import { FilterTagComponent } from './filter-tag/filter-tag.component';
 import { IconsComponent } from './icons/icons.component';
@@ -47,7 +38,6 @@ import { PageHeaderDocumentationComponent } from './page-header-documentation/pa
 import { DropdownSelectDocumentationComponent } from './dropdown-select-documentation/dropdown-select-documentation.component';
 import { PaginationDocumentationComponent } from './pagination-documentation/pagination-documentation.component';
 import { ModaldialogComponent } from './modaldialog/modaldialog.component';
-import { ExamplesListwithcardsComponent } from './examples-listwithcards/examples-listwithcards.component';
 import { GridDocumentationComponent } from './grid-documentation/grid-documentation.component';
 import { NotificationDocumentationComponent} from './notification-documentation/notification-documentation.component';
 
@@ -65,9 +55,6 @@ export const appRoutes: Routes = [
     { path: 'formattemplate', component: FormattemplateComponent },
     { path: 'headers', component: HeadersComponent },
     { path: 'loader', component: LoaderComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: 'listexample-notification', component: ListexampleComponent },
-    { path: 'listexample-notifications', component: ListexamplewithrownotificationComponent },
     { path: 'lockbutton', component: LockbuttonComponent },
     { path: 'panels', component: PanelsComponent },
     { path: 'savecancel', component: SavecancelComponent },
@@ -76,7 +63,6 @@ export const appRoutes: Routes = [
     { path: 'table', component: TableComponent },
     { path: 'theming', component: ThemingComponent },
     { path: 'titlevalue', component: TitleValueComponent },
-    { path: 'filtertextbox', component: FiltertexboxComponent },
     { path: 'pagestructure', component: PageStructureComponent },
     { path: 'card', component: CardComponent },
     { path: 'sizes', component: SizesComponent },
@@ -87,14 +73,8 @@ export const appRoutes: Routes = [
             component: MenuComponent
         }]
     },
-    { path: 'listexample-simple', component: ListcodeexampleComponent },
-    { path: 'listexample-pagination', component: ListwithpaginationcodeexampleComponent },
-    { path: 'listexample-actionbuttons', component: ListExampleWithActionButtonsComponent },
     { path: 'reactiveformsexample', component: ReactiveformsexampleComponent },
     { path: 'expandablediv', component: ExpandableDivComponent },
-    { path: 'listexample-expandable-div', component: ListexamplewithexpandabledivComponent },
-    { path: 'examples-listwithpageblock', component: ExamplesListwithpageblockComponent },
-    { path: 'examples-listwithcards', component: ExamplesListwithcardsComponent },
     { path: 'toggle-button', component: ToggleButtonComponent },
     { path: 'actionPanels', component: ActionPanelsComponent },
     { path: 'filter-tag', component: FilterTagComponent },
