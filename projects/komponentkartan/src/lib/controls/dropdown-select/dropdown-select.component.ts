d897265349dda2c945d27bb44e5587630823aba0
Arvid Sollenby <arvid.sollenby@gmail.com>
2020-12-03 08:37:36 +0100

    Bugfix för dropdown select

--- projects/komponentkartan/src/lib/controls/dropdown-select/dropdown-select.component.ts
+++ projects/komponentkartan/src/lib/controls/dropdown-select/dropdown-select.component.ts
@@ -393,7 +393,7 @@ export class DropdownSelectComponent implements OnChanges, AfterContentInit, Aft
     } else if (event.key === 'ArrowDown' || event.key === 'Down') {
       if (this.multi) {
         this.selectAll.nativeElement.focus();
-      } else if (this.deselectable) {
+      } else if (this.deselectable && this.value) {
         this.deselectButton.focus();
       } else if (filteredItems.length) {
         filteredItems[0].focus();
@@ -535,7 +535,7 @@ export class DropdownSelectComponent implements OnChanges, AfterContentInit, Aft
       filteredItems[itemIndex - 1].focus();
     } else if (this.multi) {
       this.selectAll.nativeElement.focus();
-    } else if (this.deselectable) {
+    } else if (this.deselectable && this.value) {
       this.deselectButton.focus();
     } else if (this.filterVisible) {
       this.filter.focus();
