b4d57555f0530ebd9061506de44b29b3aa189c11
Junior <albertino-junior.lopes-livramento@vgregion.se>
2021-02-09 09:46:26 +0100

    refakt av input validering samt css fixar för dropdown, datepicker och input
    Co-authored-by: Caroline Bornsjö <carbo15@vgregion.se>

--- projects/komponentkartan/src/lib/controls/input/input.component.ts
+++ projects/komponentkartan/src/lib/controls/input/input.component.ts
@@ -74,6 +74,9 @@ export class InputComponent implements ControlValueAccessor, OnChanges, OnInit, 
     }
   }
 
+
+
+
   ngOnChanges(changes: SimpleChanges) {
     if (this.control && changes.disabledControl) {
       this.setDisabledState(changes.disabledControl.currentValue);
@@ -86,6 +89,16 @@ export class InputComponent implements ControlValueAccessor, OnChanges, OnInit, 
 
   setDisabledState(isDisabled: boolean) {
     this.disabledControl = isDisabled;
+  }
+
+
+  get errorActive() {
+    if (!this.showValidation || this.disabledControl || this.readonly) {
+      return false;
+    }
+
+    return ((this.control && this.control.invalid) || (this.required && this.value == '' ));
+
   }
 
   propagateChange = (_: any) => {};
