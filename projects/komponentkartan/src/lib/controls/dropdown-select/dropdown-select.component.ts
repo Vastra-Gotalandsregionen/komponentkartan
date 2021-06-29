dcb2471aafaf7198f4047ea5c6d453541cd05084
Alexander <alexander.eliasson@vgregion.se>
2021-06-29 14:44:55 +0200

    Dropdown select med bara ett val blir readonly

--- projects/komponentkartan/src/lib/controls/dropdown-select/dropdown-select.component.ts
+++ projects/komponentkartan/src/lib/controls/dropdown-select/dropdown-select.component.ts
@@ -53,6 +53,7 @@ export class DropdownSelectComponent implements OnChanges, AfterContentInit, Aft
   deselectDisabled = true;
   headerLabelId = Guid.newGuid();
   label = this.noItemSelectedLabel;
+  readonlyNoPadding = false;
 
   hasFocus: boolean;
   filterHasFocus: boolean;
@@ -109,6 +110,10 @@ export class DropdownSelectComponent implements OnChanges, AfterContentInit, Aft
     this.subscribeToItems();
     if (this.multi) {
       this.setMultiOnItems();
+    }
+
+    if (this.items?.length === 1 && !this.deselectable && !this.disabled && !this.readonly) {
+      this.readonlyNoPadding = true;
     }
 
     this.items.changes
@@ -180,7 +185,7 @@ export class DropdownSelectComponent implements OnChanges, AfterContentInit, Aft
   onTouched() { }
 
   toggleExpanded() {
-    if (this.readonly || this.disabled) {
+    if (this.readonly || this.readonlyNoPadding || this.disabled) {
       return;
     }
 
