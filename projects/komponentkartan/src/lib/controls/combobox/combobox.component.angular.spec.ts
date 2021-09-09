0effea4c3d3886856154c4a1bd17185131fd0295
Synnöve Lysö <synnove.lyso@elicit.se>
2021-09-09 16:12:12 +0200

    Mer tester.

--- projects/komponentkartan/src/lib/controls/combobox/combobox.component.angular.spec.ts
+++ projects/komponentkartan/src/lib/controls/combobox/combobox.component.angular.spec.ts
@@ -39,10 +39,12 @@ describe('[ComboboxComponent - Angular]', () => {
   let expandButtonElement: DebugElement;
   let menuElement: DebugElement;
   let textInputElement: DebugElement;
+
   beforeAll(() => {
     jasmine.clock().uninstall();
     jasmine.clock().install();
   });
+
   afterAll(() => {
     jasmine.clock().uninstall();
   });
@@ -181,7 +183,6 @@ describe('[ComboboxComponent - Angular]', () => {
         textInputElement.triggerEventHandler('keydown', event);
       });
       it('expandedChanged is emitted', () => {
-        // textInputElement.nativeElement.dispatchEvent(new KeyboardEvent(event));
         fixture.detectChanges();
         expect(spy).toHaveBeenCalled();
       });
@@ -215,6 +216,51 @@ describe('[ComboboxComponent - Angular]', () => {
         fixture.detectChanges();
         itemElements = rootElement.queryAll(By.css('.combobox__menu__items__item'));
         expect(itemElements[0].classes['combobox-item--highlighted']).toBe(true);
+      });
+    });
+    describe('When altKey + key is ArrowUp or Up', () => {
+      beforeEach(() => {
+        const key = pickRandom(['ArrowUp', 'Up']);
+        event = { key: key, altKey: true } as KeyboardEvent;
+        textInputElement = rootElement.query(By.css('.combobox__header__input'));
+        spy = spyOn(component.expandedChanged, 'emit');
+        textInputElement.triggerEventHandler('keydown', event);
+      });
+      it('expandedChanged is emitted', () => {
+        fixture.detectChanges();
+        expect(spy).toHaveBeenCalled();
+      });
+      it('expanded to be fale', () => {
+        fixture.detectChanges();
+        expect(component.expanded).toBeFalse();
+      });
+      it('no visible items exists', () => {
+        fixture.detectChanges();
+        itemElements = rootElement.queryAll(By.css('.combobox__menu__items__item'));
+        expect(itemElements.length).toEqual(0);
+      });
+    });
+    describe('When altKey + key is ArrowDown or Down', () => {
+      beforeEach(() => {
+        const key = pickRandom(['ArrowDown', 'Down']);
+        event = { key: key, altKey: true } as KeyboardEvent;
+        textInputElement = rootElement.query(By.css('.combobox__header__input'));
+        spy = spyOn(component.expandedChanged, 'emit');
+        textInputElement.triggerEventHandler('keydown', event);
+        fixture.detectChanges();
+      });
+      it('expandedChanged is emitted', () => {
+        expect(spy).toHaveBeenCalled();
+      });
+      it('expanded to be true', () => {
+        expect(component.expanded).toBeTrue();
+      });
+      it('there is not any item higlighted', () => {
+        jasmine.clock().tick(1);
+        fixture.detectChanges();
+        itemElements = rootElement.queryAll(By.css('.combobox__menu__items__item'));
+        const index = itemElements.findIndex(x => x.classes['combobox-item--highlighted']);
+        expect(index).toEqual(-1);
       });
     });
     describe('When key is Enter', () => {
@@ -356,15 +402,17 @@ describe('[ComboboxComponent - Angular]', () => {
           jasmine.clock().tick(1);
           fixture.detectChanges();
         });
-        it('expandedChanged is emitted', () => {        
+        it('expandedChanged is emitted', () => {
           expect(spy).toHaveBeenCalled();
         });
       });
     });
     describe('When key is Tab or Shift + Tab', () => {
       let key: string;
+      let altKey: boolean;
       beforeEach(() => {
-        key = 'Tab'
+        key = 'Tab';
+        altKey = pickRandom([true, false]);
         event = { key: key } as KeyboardEvent;
       });
       describe('and there are items or not', () => {
@@ -375,10 +423,10 @@ describe('[ComboboxComponent - Angular]', () => {
           jasmine.clock().tick(1);
           fixture.detectChanges();
         });
-        it('expandedChanged is emitted', () => {        
+        it('expandedChanged is emitted', () => {
           expect(spy).toHaveBeenCalled();
         });
-        it('expanded is false', () => {        
+        it('expanded is false', () => {
           expect(component.expanded).toBeFalse();
         });
       });
