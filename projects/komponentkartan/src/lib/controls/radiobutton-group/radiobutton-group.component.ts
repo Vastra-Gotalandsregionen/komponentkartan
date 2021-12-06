ba37c4c61efa08b372fab4cb4ed18345af99276c
Junior <albertino-junior.lopes-livramento@vgregion.se>
2021-12-06 10:41:44 +0100

    WIP för disabled och selected
    Co-authored-by: Caroline Bornsjö <CrestTheFury@users.noreply.github.com>

--- projects/komponentkartan/src/lib/controls/radiobutton-group/radiobutton-group.component.ts
+++ projects/komponentkartan/src/lib/controls/radiobutton-group/radiobutton-group.component.ts
@@ -1,15 +1,58 @@
-import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, EventEmitter, HostBinding, Input, OnDestroy, Output, QueryList } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { RadiobuttonItemComponent } from './radiobutton-item.component';
 
 @Component({
   selector: 'vgr-radiobutton-group',
   templateUrl: './radiobutton-group.component.html',
   styleUrls: ['./radiobutton-group.component.scss']
 })
-export class RadiobuttonGroupComponent {
+export class RadiobuttonGroupComponent implements AfterContentInit, OnDestroy {
 
   @Input() @HostBinding('class.disabled') disabled: boolean;
   @Input() @HostBinding('class.vertical') vertical = false;
   @Output() selectedChanged: EventEmitter<any> = new EventEmitter<any>();
-  constructor() { }
+  @ContentChildren(RadiobuttonItemComponent) items: QueryList<RadiobuttonItemComponent>;
 
+  ngUnsubscribeItems = new Subject();
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterContentInit() {
+    this.items.forEach(item => {
+      item.itemSelected.pipe(
+        takeUntil(this.ngUnsubscribeItems)
+      ).subscribe(() => {
+        this.unSelectItems(item);
+      });
+
+      item.itemDisabled.pipe(
+        takeUntil(this.ngUnsubscribeItems)
+      ).subscribe(() => {
+        this.disabledItems();
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribeItems.next();
+    this.ngUnsubscribeItems.complete();
+  }
+
+  unSelectItems(itemToExclude: RadiobuttonItemComponent) {
+    this.items.forEach(item => {
+      if (item !== itemToExclude) {
+        item.selected = false;
+      }
+    });
+  }
+
+  disabledItems() {
+    this.items.forEach(item => {
+      if (this.disabled) {
+        item.disabled = true;
+      }
+    });
+  }
 }
--- projects/komponentkartan/src/lib/controls/radiobutton-group/radiobutton-item.component.ts
+++ projects/komponentkartan/src/lib/controls/radiobutton-group/radiobutton-item.component.ts
@@ -1,14 +1,34 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'vgr-radiobutton-item',
   templateUrl: './radiobutton-item.component.html',
   styleUrls: ['./radiobutton-item.component.scss']
 })
-export class RadiobuttonItemComponent {
+export class RadiobuttonItemComponent implements AfterViewInit {
 
   @Input() selected: boolean;
-  @Input() @HostBinding('class.disabled') disabled: boolean;
+  @Input() disabled: boolean;
+  @Output() itemSelected = new EventEmitter();
+  @Output() itemDisabled = new EventEmitter();
+
+  @ViewChild('item') item: ElementRef;
+
   constructor() { }
 
+  ngAfterViewInit() {
+    this.itemDisabled.emit();
+    if (this.disabled && this.selected) {
+      this.selected = false;
+    }
+  }
+
+  itemClicked() {
+    if (this.item && !this.disabled) {
+      this.item.nativeElement.focus();
+      this.selected = true;
+      this.itemSelected.emit();
+      return;
+    }
+  }
 }
