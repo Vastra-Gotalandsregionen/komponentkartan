807aff0479f40b3ac886b0712ccb4011ee358d2f
Junior <albertino-junior.lopes-livramento@vgregion.se>
2022-02-11 07:23:14 +0100

    skrivit en del tester

--- projects/komponentkartan/src/lib/controls/checkbox/checkbox-group.component.spec.ts
+++ projects/komponentkartan/src/lib/controls/checkbox/checkbox-group.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckboxGroupComponent } from './checkbox-group.component';
 
-describe('CheckboxGroupComponent', () => {
+fdescribe('CheckboxGroupComponent', () => {
   let component: CheckboxGroupComponent;
   let fixture: ComponentFixture<CheckboxGroupComponent>;
 
@@ -21,5 +21,41 @@ describe('CheckboxGroupComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Initial values for checkbox-group', () => {
+    let setGroupDisabledSpy;
+    let onChangeSpy;
+    beforeEach(() => {
+      setGroupDisabledSpy = spyOn(component, 'setGroupDisabledOverride').and.callThrough();
+      onChangeSpy = spyOn(component, 'onChange').and.callThrough();
+      component.disabled = true;
+      component.ngAfterContentInit();
+      fixture.detectChanges();
+    });
+
+    it('setGroupDisabledOverride() should be called 2 times', () => {
+      expect(setGroupDisabledSpy).toHaveBeenCalledTimes(2);
+    });
+
+    it('onChange() should be called', () => {
+      expect(onChangeSpy).toHaveBeenCalled();
+    });
+
+    it('required should be false', () => {
+      expect(component.required).toBe(false);
+    });
+
+    it('showValidation should be true', () => {
+      expect(component.showValidation).toBe(true);
+    });
+
+    it('vertical should be false', () => {
+      expect(component.vertical).toBe(false);
+    });
+
+    it('_disabled should be true', () => {
+      expect(component._disabled).toBe(true);
+    });
   });
 });
