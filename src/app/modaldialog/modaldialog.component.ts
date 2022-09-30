d4d092d1cf595e5d96ce0a22dda4a130b62cd710
Junior <albertino-junior.lopes-livramento@vgregion.se>
2022-09-30 07:17:21 +0200

    uppdaterat forms för modaldialog

--- src/app/modaldialog/modaldialog.component.ts
+++ src/app/modaldialog/modaldialog.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HtmlEncodeService } from '../html-encode.service';
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ModalService } from '../../../projects/komponentkartan/src/lib';
-
-declare var tinymce: any;
 
 @Component({
   selector: 'app-modaldialog',
@@ -11,8 +9,8 @@ declare var tinymce: any;
   styleUrls: ['./modaldialog.component.scss']
 })
 export class ModaldialogComponent implements OnInit {
-  vardvalForm: UntypedFormGroup;
-  vardvalForm2: UntypedFormGroup;
+  vardvalForm: FormGroup;
+  vardvalForm2: FormGroup;
   vardval1Answer: string;
   vardval2Answer: string;
   validateOnSubmit: boolean;
@@ -49,7 +47,7 @@ export class ModaldialogComponent implements OnInit {
 
   exampleCodeMarkup: string;
   exampleCodeTypescript: string;
-  form: UntypedFormGroup;
+  form: FormGroup;
 
   constructor(public modalService: ModalService, htmlEncoder: HtmlEncodeService) {
     this.exampleCodeMarkup =
@@ -122,16 +120,16 @@ export class ModaldialogComponent implements OnInit {
   }
 
   createForms() {
-    this.vardvalForm = new UntypedFormGroup({
-      vardval: new UntypedFormControl(null, { validators: [Validators.required] }),
+    this.vardvalForm = new FormGroup({
+      vardval: new FormControl(null, { validators: [Validators.required] }),
     }, { updateOn: 'change' });
 
-    this.vardvalForm2 = new UntypedFormGroup({
-      vardval: new UntypedFormControl(null, { validators: [Validators.required] }),
+    this.vardvalForm2 = new FormGroup({
+      vardval: new FormControl(null, { validators: [Validators.required] }),
     }, { updateOn: 'change' });
 
-    this.form = new UntypedFormGroup({
-      textarea: new UntypedFormControl('', { validators: [Validators.required] })
+    this.form = new FormGroup({
+      textarea: new FormControl('', { validators: [Validators.required] })
     }, { updateOn: 'blur' });
   }
 
