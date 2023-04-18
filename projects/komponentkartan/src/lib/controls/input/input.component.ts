52acdec0ea46b4600bbe8403fac6db89140c96a4
Junior <albertino-junior.lopes-livramento@vgregion.se>
2023-04-18 15:25:41 +0200

    fix för dropdown expressionchanged
    Co-authored-by: Caroline Bornsjö <caroline.bornsjo@vgregion.se>

--- projects/komponentkartan/src/lib/controls/input/input.component.ts
+++ projects/komponentkartan/src/lib/controls/input/input.component.ts
@@ -65,9 +65,7 @@ export class InputComponent implements ControlValueAccessor, OnChanges, OnInit, 
       this.hasSuffix = true;
     }
     if (this.formControlName) {
-      console.log('formControlName', this.formControlName)
       this.control = this.controlContainer.control.get(this.formControlName);
-      console.log('control', this.control)
     }
   }
 
