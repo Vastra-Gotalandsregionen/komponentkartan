607021f18ce8b7fb86f08f206520f85941c8d8df
Junior Lopes <albertino-junior.lopes-livramento@vgregion.se>
2023-10-18 14:04:52 +0200

    lagt till ytterligare dokumentation och exempel

--- src/app/selectablelist-documentation/selectablelist-documentation.component.ts
+++ src/app/selectablelist-documentation/selectablelist-documentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HtmlEncodeService } from '../html-encode.service';
 
 @Component({
   selector: 'vgr-selectablelist-documentation',
@@ -60,7 +61,29 @@ export class SelectablelistDocumentationComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  exampleCode = `<vgr-selectablelist [id]="'test'" [active]="true">
+      <vgr-selectablelist-header>
+          <vgr-selectablelist-header-column><strong>Utbetalning avser</strong>
+          </vgr-selectablelist-header-column>
+          <vgr-selectablelist-header-column><strong>Månad</strong></vgr-selectablelist-header-column>
+          <vgr-selectablelist-header-column [alignRight]="true"><strong>Belopp</strong>
+          </vgr-selectablelist-header-column>
+        </vgr-selectablelist-header>
+      <vgr-selectablelist-row *ngFor="let justering of justeringar; let i = index" [value]="i">
+        <vgr-selectablelist-column style="max-width: 350px; width:350px">{{justering.beskrivning}}...
+        </vgr-selectablelist-column>
+        <vgr-selectablelist-column>{{justering.period | date: 'MMM YYYY'}}
+        </vgr-selectablelist-column>
+        <vgr-selectablelist-column [alignRight]="true">{{justering.belopp}}
+        </vgr-selectablelist-column>
+      </vgr-selectablelist-row>
+  </vgr-selectablelist>`;
+  exampleCodeMarkup: string = '';
+
+  constructor(htmlEncoder: HtmlEncodeService) {
+    this.exampleCodeMarkup =
+      htmlEncoder.prepareHighlightedSection(this.exampleCode, 'HTML');
+   }
 
   ngOnInit(): void {
   }
